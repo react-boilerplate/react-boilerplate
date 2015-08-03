9d1d54774eb627f1bd716399d23b942353c7d12b
Max <contact@mstoiber.com>
2015-08-03 14:11:52 +0200

    Fix App not reacting to data changes.

--- js/components/App.react.js
+++ js/components/App.react.js
@@ -5,11 +5,20 @@ var App = React.createClass({
 	getInitialState: function() {
 		return AppStore.getData();
 	},
+	componentDidMount: function() {
+		AppStore.addChangeListener(this._onChange);
+	},
+	componentWillUnmount: function() {
+		AppStore.removeChangeListener(this._onChange);
+	},
 	render: function() {
 		AppActions.defaultAction(false);
 		return(
 			<h1>Hello World! Default: { this.state.default.toString() }</h1>
 		);
+	},
+	_onChange: function() {
+	    this.setState(AppStore.getData());
 	}
 });
 
