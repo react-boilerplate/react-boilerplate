e698440e565dc7030739c2578ee992cd554fdb3c
Max <contact@mstoiber.com>
2015-08-05 17:17:13 +0200

    Added hot reloading
    To add hot reloading with Dan Abramov's brilliant react-hot-loader, I had to move from the previous Grunt+Browserify toolchain to webpack. Webpack can now do the exact same thing the previous setup did, except for grunt build which I am currently working on.

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ import App from './components/App.react';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { defaultApp } from './reducers/reducers';
+import '../css/main.css';
 
 const store = createStore(defaultApp);
 
--- js/components/App.react.js
+++ js/components/App.react.js
@@ -13,7 +13,7 @@ class App extends Component {
 				</button>
 			</div>
 		);
-  }
+  	}
 }
 
 App.propTypes = {
