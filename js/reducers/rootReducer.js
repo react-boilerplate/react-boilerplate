6ca7d8ed1bdf9bf43f9e713766f5fb626da2c7f5
Max <contact@mstoiber.com>
2015-12-19 11:42:37 +0100

    Fix eslint error

--- js/reducers/rootReducer.js
+++ js/reducers/rootReducer.js
@@ -3,10 +3,10 @@
  * If we were to do this in store.js, reducers wouldn't be hot reloadable.
  */
 
-import { combineReducers } from 'redux';
 import homeReducer from './homeReducer';
 
 // Replace line below once you have several reducers with
+// import { combineReducers } from 'redux';
 // const rootReducer = combineReducers({ homeReducer, yourReducer })
 const rootReducer = homeReducer;
 
