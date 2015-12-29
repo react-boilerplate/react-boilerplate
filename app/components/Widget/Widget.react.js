a7bcb2ae2396f4ba821fa498498cf84c33665416
Philihp Busby <philihp@gmail.com>
2015-12-29 14:05:32 -0800

    Move Widget to its own folder

--- app/components/Widget/Widget.react.js
+++ app/components/Widget/Widget.react.js
@@ -4,6 +4,9 @@ const propTypes = {
   name: PropTypes.string.isRequired
 };
 
+// Normally you would use a function, as this is a stateless functional
+// component, however for the purposes of demonstrating a proper Enzyme test, we
+// make this a proper component.
 class Widget extends Component {
   render() {
     return (
