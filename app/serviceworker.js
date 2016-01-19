0bb546360a6d03c62acd5df251821f6638cd7342
Hosmel Quintana 😴 <hosmelq@gmail.com>
2016-01-19 11:27:35 -0600

    fix the main js file reference in sw

--- app/serviceworker.js
+++ app/serviceworker.js
@@ -3,7 +3,7 @@ const CACHE_NAME = 'react-boilerplate-cache-v1';
 const urlsToCache = [
   '/',
   '/css/main.css',
-  '/js/bundle.js'
+  '/js/main.js'
 ];
 
 // Set the callback for the install step
