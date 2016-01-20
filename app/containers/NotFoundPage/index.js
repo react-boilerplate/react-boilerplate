7130ccba11567e480434080cc51d50d4e60b8ad4
Max <contact@mstoiber.com>
2016-01-20 22:57:46 +0000

    Fix NotFoundPage button

--- app/containers/NotFoundPage/index.js
+++ app/containers/NotFoundPage/index.js
@@ -1,14 +1,37 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { routeActions } from 'redux-simple-router';
 
 import Button from 'Button';
 
-function NotFound() {
+function NotFound(props) {
+  console.log(props);
   return (
     <article>
       <h1>Page not found.</h1>
-      <Button route="/">Home</Button>
+      <Button
+        handleRoute={function redirect() {
+          props.changeRoute('/');
+        }}
+      >
+        Home
+      </Button>
     </article>
   );
 }
 
-export default NotFound;
+// react-redux stuff
+function mapStateToProps(state) {
+  return {
+    location: state.routing.location
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    changeRoute: (url) => dispatch(routeActions.push(url))
+  };
+}
+
+// Wrap the component to inject dispatch and state into it
+export default connect(mapStateToProps, mapDispatchToProps)(NotFound);
