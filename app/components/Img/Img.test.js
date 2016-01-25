4ac051b4c454f5ae34fc6d15f08ce5890f1d63c9
Jesse Buchanan <hello@jessebuchanan.ca>
2016-01-24 21:49:41 -0500

    Revert "Show a test using Chai-Enzyme"
    This reverts commit d91c8434e6b9d67e5c8492d2796f5f2b51474b00.

--- app/components/Img/Img.test.js
+++ app/components/Img/Img.test.js
@@ -1,17 +1,17 @@
 import Img from './index';
 
-import { expect } from 'chai';
+import expect from 'expect';
 import { shallow } from 'enzyme';
 import React from 'react';
 
 describe('<Img />', () => {
   it('should render an <img> tag', () => {
     const renderedComponent = shallow(<Img src="test.png" alt="test" />);
-    expect(renderedComponent).to.have.tagName('img');
+    expect(renderedComponent.find('img').length).toEqual(1);
   });
 
   it('should have an alt attribute', () => {
     const renderedComponent = shallow(<Img src="test.png" alt="test" />);
-    expect(renderedComponent).to.have.attr('alt', 'test');
+    expect(renderedComponent.prop('alt')).toEqual('test');
   });
 });
--- webpack/testHelper.js
+++
@@ -1,4 +0,0 @@
-import chai from 'chai';
-import chaiEnzyme from 'chai-enzyme';
-
-chai.use(chaiEnzyme());
