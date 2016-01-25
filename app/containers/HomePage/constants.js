0793af315c724691ba297bdb67aa475c9592d9ab
Max <contact@mstoiber.com>
2016-01-25 13:02:36 +0100

    Prefix constants

--- app/containers/HomePage/constants.js
+++ app/containers/HomePage/constants.js
@@ -4,7 +4,9 @@
  * changes in our state. When you add a new action, you have to add a new constant here
  *
  * Follow this format:
- * export const YOUR_ACTION_CONSTANT = 'YOUR_ACTION_CONSTANT';
+ * export const YOUR_ACTION_CONSTANT = 'projectName/componentName/YOUR_ACTION_CONSTANT';
+ *
+ * The constants are prefixed so there cannot be overlap between constants in different reducers/components
  */
-export const CHANGE_PROJECT_NAME = 'CHANGE_PROJECT_NAME';
-export const CHANGE_OWNER_NAME = 'CHANGE_OWNER_NAME';
+export const CHANGE_PROJECT_NAME = 'boilerplate/HomePage/CHANGE_PROJECT_NAME';
+export const CHANGE_OWNER_NAME = 'boilerplate/HomePage/CHANGE_OWNER_NAME';
