ccd4f6cf81d76c360b7b983fc8ef9796c3c07b1a
Max <contact@mstoiber.com>
2016-02-07 19:06:56 +0100

    Move routes to their own file
    Closes #154

---
+++ app/routes.js
@@ -0,0 +1,31 @@
+// These are the pages you can go to.
+// They are all wrapped in the App component, which contains the navigation etc
+// See http://blog.mxstbr.com/2016/01/react-apps-with-pages for more information
+// about the require.ensure code splitting business
+
+const routes = [
+  {
+    path: '/',
+    getComponent: function get(location, cb) {
+      require.ensure([], (require) => {
+        cb(null, require('HomePage').default);
+      }, 'HomePage');
+    }
+  }, {
+    path: '/features',
+    getComponent: function get(location, cb) {
+      require.ensure([], (require) => {
+        cb(null, require('FeaturePage').default);
+      }, 'FeaturePage');
+    }
+  }, {
+    path: '*',
+    getComponent: function get(location, cb) {
+      require.ensure([], (require) => {
+        cb(null, require('NotFoundPage').default);
+      }, 'NotFoundPage');
+    }
+  }
+];
+
+export default routes;
