061d835a527c895ef579ba73b540905fdc61494f
Jesse Buchanan <hello@jessebuchanan.ca>
2016-02-07 18:21:47 -0500

    Separate lcov and html coverage reports

--- webpack/karma.conf.js
+++ webpack/karma.conf.js
@@ -41,7 +41,8 @@ module.exports = (config) => {
     coverageReporter: {
       dir: '../coverage',
       reporters: [
-        { type: 'lcov', subdir: 'report-lcov' }
+        { type: 'lcov', subdir: 'lcov' },
+        { type: 'html', subdir: 'html' }
       ]
     },
 
