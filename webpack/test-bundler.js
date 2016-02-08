3b43353dc2779b566d24b7df79ff1eb1d3177ac9
Jesse Buchanan <hello@jessebuchanan.ca>
2016-02-08 12:50:16 -0500

    make eslint happy

--- webpack/test-bundler.js
+++ webpack/test-bundler.js
@@ -1,6 +1,6 @@
 // needed for regenerator-runtime
 // (ES7 generator support is required by redux-saga)
-import "babel-polyfill";
+import 'babel-polyfill';
 
 import sinon from 'sinon';
 import chai from 'chai';
