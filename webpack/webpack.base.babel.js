638596fd1d0d9810f5dce12e30060d1edbf88288
Max <contact@mstoiber.com>
2016-02-12 11:50:26 -0700

    Update to eslint v2

--- webpack/webpack.base.babel.js
+++ webpack/webpack.base.babel.js
@@ -5,58 +5,54 @@
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = (options) => {
-  return {
-    entry: options.entry,
-    output: { // Compile into js/build.js
-      path: path.resolve(__dirname, '..', 'build'),
-      filename: '[name].js',
-      chunkFilename: '[name].chunk.js',
-      publicPath: '/'
-    },
-    module: {
-      loaders: [{
-        test: /\.js$/, // Transform all .js files required somewhere with Babel
-        loader: 'babel',
-        exclude: path.join(__dirname, '..', '/node_modules/'),
-        query: options.babelQuery,
-      }, {
-        test: /\.css$/, // Transform all .css files required somewhere with PostCSS
-        loader: options.cssLoaders
-      }, {
-        test: /\.jpe?g$|\.gif$|\.png$/i,
-        loader: 'url-loader?limit=10000'
-      }, {
-        test: /\.html$/,
-        loader: 'html-loader'
-      }
-      ]
-    },
-    plugins: options.plugins.concat([
-      new webpack.optimize.CommonsChunkPlugin('common.js')
-    ]),
-    postcss: () => {
-      return options.postcssPlugins;
-    },
-    resolve: {
-      modulesDirectories: [
-        'containers',
-        'components',
-        'selectors',
-        'sagas',
-        'assets',
-        'node_modules'
-      ],
-      extensions: [
-        '',
-        '.js',
-        '.jsx',
-        '.react.js'
-      ]
-    },
-    devtool: options.devtool,
-    target: 'web', // Make web variables accessible to webpack, e.g. window
-    stats: false, // Don't show stats in the console
-    progress: true
-  };
-};
+module.exports = (options) => ({
+  entry: options.entry,
+  output: { // Compile into js/build.js
+    path: path.resolve(__dirname, '..', 'build'),
+    filename: '[name].js',
+    chunkFilename: '[name].chunk.js',
+    publicPath: '/'
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/, // Transform all .js files required somewhere with Babel
+      loader: 'babel',
+      exclude: path.join(__dirname, '..', '/node_modules/'),
+      query: options.babelQuery,
+    }, {
+      test: /\.css$/, // Transform all .css files required somewhere with PostCSS
+      loader: options.cssLoaders
+    }, {
+      test: /\.jpe?g$|\.gif$|\.png$/i,
+      loader: 'url-loader?limit=10000'
+    }, {
+      test: /\.html$/,
+      loader: 'html-loader'
+    }
+    ]
+  },
+  plugins: options.plugins.concat([
+    new webpack.optimize.CommonsChunkPlugin('common.js')
+  ]),
+  postcss: () => options.postcssPlugins,
+  resolve: {
+    modulesDirectories: [
+      'containers',
+      'components',
+      'selectors',
+      'sagas',
+      'assets',
+      'node_modules'
+    ],
+    extensions: [
+      '',
+      '.js',
+      '.jsx',
+      '.react.js'
+    ]
+  },
+  devtool: options.devtool,
+  target: 'web', // Make web variables accessible to webpack, e.g. window
+  stats: false, // Don't show stats in the console
+  progress: true
+});
