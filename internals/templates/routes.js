26b4e691573a7337d00a01c1c555b6c5a19a6f1f
Valentin Semirulnik <v7rulnik@gmail.com>
2016-04-05 23:50:33 +0300

    add injectAsyncReducer into template

--- internals/templates/routes.js
+++ internals/templates/routes.js
@@ -4,6 +4,7 @@
 // They are all wrapped in the App component, which should contain the navbar etc
 // See http://blog.mxstbr.com/2016/01/react-apps-with-pages for more information
 // about the require.ensure code splitting business
+import { injectAsyncReducer } from './store';
 
 function errorLoading(err) {
   console.error('Dynamic page loading failed', err);
@@ -13,11 +14,19 @@ function loadModule(cb) {
   return (module) => cb(null, module.default);
 }
 
+function loadReducer(store, name) {
+  return (module) => injectAsyncReducer(store, name, module.default);
+}
+
 export default function createRoutes(store) { // eslint-disable-line
   return [
     {
       path: '/',
       getComponent(location, cb) {
+        System.import('HomePage/reducer')
+          .then(loadReducer(store, 'home'))
+          .catch(errorLoading);
+
         System.import('HomePage')
           .then(loadModule(cb))
           .catch(errorLoading);
