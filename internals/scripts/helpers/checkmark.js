75a8afaffd73a00ac0ea0450f1aeacd91ed17e83
Max <contact@mstoiber.com>
2016-04-13 11:19:04 +0200

    Move common cli helpers to own folder

---
+++ internals/scripts/helpers/checkmark.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+var chalk = require('chalk');
+
+/**
+ * Adds mark check symbol
+ */
+function addCheckMark(callback) {
+  process.stdout.write(chalk.green(' ✓'));
+  callback();
+}
+
+module.exports = addCheckMark;
