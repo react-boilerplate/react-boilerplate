191b4533246ee738d8c80b22a8153b2e0ebc3fbb
Max <contact@mstoiber.com>
2016-05-15 19:48:37 +0200

    Remove deprecated saga generator

--- internals/generators/index.js
+++ internals/generators/index.js
@@ -6,12 +6,10 @@
 
 const componentGenerator = require('./component/index.js');
 const containerGenerator = require('./container/index.js');
-const sagaGenerator = require('./saga/index.js');
 const routeGenerator = require('./route/index.js');
 
 module.exports = (plop) => {
   plop.setGenerator('component', componentGenerator);
   plop.setGenerator('container', containerGenerator);
-  plop.setGenerator('saga', sagaGenerator);
   plop.setGenerator('route', routeGenerator);
 };
