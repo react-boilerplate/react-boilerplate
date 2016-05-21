a2e55457dc35f9f6ebd6f6a8d188ca9dfc3c263c
Brandon Dail <Kierkegaurd@gmail.com>
2016-05-21 12:04:52 -0500

    Linting fix and explaination
    Additional linting fixes

--- internals/templates/appContainer.js
+++ internals/templates/appContainer.js
@@ -4,10 +4,16 @@
  *
  * This component is the skeleton around the actual pages, and should only
  * contain code that should be seen on all pages. (e.g. navigation bar)
+ *
+ * NOTE: while this component should technically be a stateless functional
+ * component (SFC), hot reloading does not currently support SFCs. If hot
+ * reloading is not a neccessity for you then you can refactor it and remove
+ * the linting exception.
  */
 
 import React from 'react';
 
+/* eslint-disable react/prefer-stateless-function */
 export default class App extends React.Component {
 
   static propTypes = {
@@ -17,7 +23,7 @@ export default class App extends React.Component {
   render() {
     return (
       <div>
-        {props.children}
+        {this.props.children}
       </div>
     );
   }
