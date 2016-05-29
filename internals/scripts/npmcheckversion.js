a908de0cc7c19487eed75b0e476a4c56809cde9a
Rogelio Alberto <i@rog.mx>
2016-05-29 01:13:32 -0500

    Don\'t complicate my life and only exec npm -v for crossplataform

--- internals/scripts/npmcheckversion.js
+++ internals/scripts/npmcheckversion.js
@@ -1,12 +1,9 @@
 /* eslint-disable */
-if (!/^win/.test(process.platform)) {
-  var exec = require('child_process').exec;
-  exec("if [ $(npm -v | cut -c 1 | awk '{print $1\"<3\"}' | bc -l) != 0 ]; then echo \"[ERROR: React Boilerplate] You need npm version @>=3\n\"; fi;",
-    function (error, stdout, stderr) {
-      console.log(stdout);
-      if (stdout !== '') {
-        process.exit(1);
-      }
-    }
-  );
-}
+var exec = require('child_process').exec;
+exec('npm -v', function (err, stdout, stderr) {
+  if (err) throw err;
+  if (parseFloat(stdout) < 3) {
+    throw new Error('[ERROR: React Boilerplate] You need npm version @>=3');
+    process.exit(1);
+  }
+});
