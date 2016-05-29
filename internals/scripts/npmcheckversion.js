9f43eb8cfdefb70e8aa3740ada3d86ba72c17a91
Rogelio Alberto <i@rog.mx>
2016-05-28 23:22:37 -0500

    Linter

--- internals/scripts/npmcheckversion.js
+++ internals/scripts/npmcheckversion.js
@@ -1,12 +1,12 @@
+/* eslint-disable */
 if (!/^win/.test(process.platform)) {
-  var sys = require("util")
-  var exec = require("child_process").exec
+  var exec = require('child_process').exec;
   exec("if [ $(npm -v | cut -c 1 | awk '{print $1\"<3\"}' | bc -l) != 0 ]; then echo \"[ERROR: React Boilerplate] You need npm version @>=3\n\"; fi;",
-    function(error, stdout, stderr) {
-      console.log(stdout)
-      if (stdout!== 0) {
-        process.exit(1)
+    function (error, stdout, stderr) {
+      console.log(stdout);
+      if (stdout !== '') {
+        process.exit(1);
       }
     }
-  )
+  );
 }
