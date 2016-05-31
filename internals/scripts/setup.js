80da0c517e94b9c78614773b94be10427f4083fc
FezVrasta <federico.zivolo@gmail.com>
2016-05-31 16:49:29 +0200

    re install pre-commit after setup
    fixes #371

--- internals/scripts/setup.js
+++ internals/scripts/setup.js
@@ -38,8 +38,16 @@ cleanRepo(dir, function () {
       process.stdout.write('Initialising new repository');
       initGit(dir, function () {
         clearInterval(interval);
-        process.stdout.write('\nDone!');
-        process.exit(0);
+
+        process.stdout.write('\n');
+        interval = animateProgress('Initialising pre-commit');
+        process.stdout.write('Initialising pre-commit');
+        installPreCommit(function () {
+          clearInterval(interval);
+
+          process.stdout.write('\nDone!');
+          process.exit(0);
+        });
       });
     });
   });
@@ -57,6 +65,13 @@ function cleanRepo(dir, callback) {
  */
 function initGit(dir, callback) {
   exec('git init && git add . && git commit -m "Initial commit"', addCheckMark.bind(null, callback));
+}
+
+/**
+ * Install pre-commit, needed because we have just deleted the old git repository
+ */
+function installPreCommit(callback) {
+  exec('npm install pre-commit', addCheckMark.bind(null, callback));
 }
 
 /**
