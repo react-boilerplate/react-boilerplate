e60409929a458e695a4d5e4da27628a6fe891fa0
Max <contact@mstoiber.com>
2016-06-04 21:40:05 +0200

    Fix startup problem
    Closes #389

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -33,7 +33,10 @@ module.exports = (options) => ({
       loaders: ['style-loader', 'css-loader'],
     }, {
       test: /\.(eot|svg|ttf|woff|woff2)$/,
-      loader : 'file-loader'
+      loader: 'file-loader',
+    }, {
+      test: /\.(jpg|png|gif)$/,
+      loader: 'file-loader',
     }, {
       test: /\.html$/,
       loader: 'html-loader',
