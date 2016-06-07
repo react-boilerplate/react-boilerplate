9a7967ec57a6784c64dedce7f7f5cc9c316a9e38
Nicolas Dupont <npg.dupont@gmail.com>
2016-06-07 15:26:14 +0200

    Add image compression to webpack process

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -36,7 +36,10 @@ module.exports = (options) => ({
       loader: 'file-loader',
     }, {
       test: /\.(jpg|png|gif)$/,
-      loader: 'file-loader',
+      loaders: [
+        'file-loader',
+        'image-webpack?{progressive:true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "65-90", speed: 4}}',
+      ],
     }, {
       test: /\.html$/,
       loader: 'html-loader',
