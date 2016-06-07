c02ea4095a468f60356c547f73bc1b88dc20ed0c
Jonathan Soeder <jonathan.soeder@gmail.com>
2016-06-07 04:23:14 -0500

    Update production CommonsChunkPlugin config

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -42,7 +42,11 @@ module.exports = require('./webpack.base.babel')({
     }),
   ],
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin('common'),
+    new webpack.optimize.CommonsChunkPlugin({
+      children: true,
+      minChunks: 2,
+      async: true
+    }),
 
     // OccurrenceOrderPlugin is needed for long-term caching to work properly.
     // See http://mxs.is/googmv
