d6fac476d9746b62899f696810f98b548ea88bd0
FezVrasta <federico.zivolo@gmail.com>
2016-06-11 15:48:10 +0200

    removed useless stuff (Chai, Chai Enzyme, global vars)

--- internals/testing/test-bundler.js
+++ internals/testing/test-bundler.js
@@ -2,17 +2,6 @@
 // (ES7 generator support is required by redux-saga)
 import 'babel-polyfill';
 
-import sinon from 'sinon';
-import chai from 'chai';
-import chaiEnzyme from 'chai-enzyme';
-
-chai.use(chaiEnzyme());
-
-global.chai = chai;
-global.sinon = sinon;
-global.expect = chai.expect;
-global.should = chai.should();
-
 // Include all .js files under `app`, except app.js, reducers.js, routes.js and
 // store.js. This is for isparta code coverage
 const context = require.context('../../app', true, /^^((?!(app|reducers|routes|store)).)*\.js$/);
