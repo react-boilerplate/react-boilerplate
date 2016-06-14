9a96a6bcf20e3d01323e3152dccba8b002ed8788
Jonathan Soeder <jonathan.soeder@gmail.com>
2016-06-13 21:41:32 -0500

    use minimist to determine the port

--- server/index.js
+++ server/index.js
@@ -3,24 +3,20 @@
 const express = require('express');
 const logger = require('./logger');
 
-const argv = require('minimist').parse(process.argv.slice(2));
-const frontend = require('./middlewares/frontendMiddleware');
+const argv = require('minimist')(process.argv.slice(2));
+const setup = require('./middlewares/frontendMiddleware');
 const isDev = process.env.NODE_ENV !== 'production';
 const ngrok = (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel ? require('ngrok') : false;
 const resolve = require('path').resolve;
-
-const app = express();
 
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
 // app.use('/api', myApi);
 
 // In production we need to pass these values in instead of relying on webpack
-app.use(
- frontend({
-   outputPath: resolve(process.cwd(), 'build'),
-   publicPath: '/',
- })
-);
+const app = setup(express(), {
+  outputPath: resolve(process.cwd(), 'build'),
+  publicPath: '/',
+});
 
 // get the intended port number, use port 3000 if not provided
 const port = argv.port || process.env.PORT || 3000;
