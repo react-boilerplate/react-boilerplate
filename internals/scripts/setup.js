df9390728b1b5f4ba2a5dab3c92ef7c68f0af104
zanedev <zane@zanedev.com>
2016-06-14 16:27:58 -0700

    fixing eslint errors

--- internals/scripts/setup.js
+++ internals/scripts/setup.js
@@ -1,47 +1,14 @@
 #!/usr/bin/env node
 
-var exec = require('child_process').exec;
-var path = require('path');
-var fs   = require('fs');
-var animateProgress = require('./helpers/progress');
-var addCheckMark = require('./helpers/checkmark');
-var readline = require('readline');
+const exec = require('child_process').exec;
+const path = require('path');
+const fs = require('fs');
+const animateProgress = require('./helpers/progress');
+const addCheckMark = require('./helpers/checkmark');
+const readline = require('readline');
 
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
-
-var dir = process.cwd();
-
-process.stdout.write('\n');
-var interval = animateProgress('Cleaning old repository');
-process.stdout.write('Cleaning old repository');
-cleanRepo(dir, function () {
-  clearInterval(interval);
-  process.stdout.write('\nInstalling dependencies... (This might take a while)');
-  setTimeout(function () {
-    readline.cursorTo(process.stdout, 0);
-    interval = animateProgress('Installing dependencies');
-  }, 500);
-
-  process.stdout.write('Installing dependencies');
-  installDeps(function (error) {
-    clearInterval(interval);
-    if (error) {
-      process.stdout.write(error);
-    }
-
-    deleteFileInCurrentDir('setup.js', function () {
-      process.stdout.write('\n');
-      interval = animateProgress('Initialising new repository');
-      process.stdout.write('Initialising new repository');
-      initGit(dir, function () {
-        clearInterval(interval);
-        process.stdout.write('\nDone!');
-        process.exit(0);
-      });
-    });
-  });
-});
 
 /**
  * Deletes the .git folder in dir
@@ -70,3 +37,35 @@ function deleteFileInCurrentDir(file, callback) {
 function installDeps(callback) {
   exec('npm install', addCheckMark.bind(null, callback));
 }
+
+const dir = process.cwd();
+process.stdout.write('\n');
+let interval = animateProgress('Cleaning old repository');
+process.stdout.write('Cleaning old repository');
+cleanRepo(dir, () => {
+  clearInterval(interval);
+  process.stdout.write('\nInstalling dependencies... (This might take a while)');
+  setTimeout(() => {
+    readline.cursorTo(process.stdout, 0);
+    interval = animateProgress('Installing dependencies');
+  }, 500);
+
+  process.stdout.write('Installing dependencies');
+  installDeps((error) => {
+    clearInterval(interval);
+    if (error) {
+      process.stdout.write(error);
+    }
+
+    deleteFileInCurrentDir('setup.js', () => {
+      process.stdout.write('\n');
+      interval = animateProgress('Initialising new repository');
+      process.stdout.write('Initialising new repository');
+      initGit(dir, () => {
+        clearInterval(interval);
+        process.stdout.write('\nDone!');
+        process.exit(0);
+      });
+    });
+  });
+});
