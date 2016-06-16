4e5bd02af6cbc98f9dace92c6fb78f7021663b15
Ravi Sankar <ravi.sankar@clara.com>
2016-06-16 15:25:08 -0700

    Support non-standard npm aliases
    Unlike node, npm doesn't have great support for transparently running
    multiple versions in parallel. Instead, a common way to run npm3
    and npm2 in parallel is to use one to globally install the other,
    with a separate alias (https://www.npmjs.com/package/npm3).
    
    The boilerplate currently explicitly uses the binary named "npm" to
    run scripts, rather than using the npm binary that was used to
    invoke the script. So "npm3 run npmcheckversion" can fail with
    a complaint that you need to be using npm v3.
    
    This change uses the npm_execpath environment variable to run
    npm scripts with whichever npm binary was used to invoke the script.

--- internals/scripts/npmcheckversion.js
+++ internals/scripts/npmcheckversion.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 var exec = require('child_process').exec;
-exec('npm -v', function (err, stdout, stderr) {
+var npmBin = process.env.npm_execpath;
+exec(npmBin + ' -v', function (err, stdout, stderr) {
   if (err) throw err;
   if (parseFloat(stdout) < 3) {
     throw new Error('[ERROR: React Boilerplate] You need npm version @>=3');
