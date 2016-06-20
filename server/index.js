fff033284c510341b3a1a3ff6cec84a8b6082a4b
Simon Selg <simon@simonselg.me>
2016-06-20 04:30:39 +0200

    Fix comment about how to use custom middlewares (#573)
    Fix comment about how to use custom middlewares
    
    This fixes #562
    
    Fix comment about how to use custom middlewares
    
    This fixes #562
    
    Fix location of comment
    
    The custom middleware has to be applied before the boilerplate specific middleware is applied

--- server/index.js
+++ server/index.js
@@ -9,11 +9,13 @@ const isDev = process.env.NODE_ENV !== 'production';
 const ngrok = (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel ? require('ngrok') : false;
 const resolve = require('path').resolve;
 
+// In production we need to pass these values in instead of relying on webpack
+const app = express();
+
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
 // app.use('/api', myApi);
 
-// In production we need to pass these values in instead of relying on webpack
-const app = setup(express(), {
+setup(app, {
   outputPath: resolve(process.cwd(), 'build'),
   publicPath: '/',
 });
