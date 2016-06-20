c9807b5f29a31b67232028ac8000ec5df2b7e0af
Max <contact@mstoiber.com>
2016-06-20 17:16:19 +0200

    Move comment in server/index.js to correct location

--- server/index.js
+++ server/index.js
@@ -8,13 +8,12 @@ const setup = require('./middlewares/frontendMiddleware');
 const isDev = process.env.NODE_ENV !== 'production';
 const ngrok = (isDev && process.env.ENABLE_TUNNEL) || argv.tunnel ? require('ngrok') : false;
 const resolve = require('path').resolve;
-
-// In production we need to pass these values in instead of relying on webpack
 const app = express();
 
 // If you need a backend, e.g. an API, add your custom backend-specific middleware here
 // app.use('/api', myApi);
 
+// In production we need to pass these values in instead of relying on webpack
 setup(app, {
   outputPath: resolve(process.cwd(), 'build'),
   publicPath: '/',
