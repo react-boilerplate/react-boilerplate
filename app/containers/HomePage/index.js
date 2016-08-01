37eec3a9e2d3423588d99c824751c257f5117077
Justin Greenberg <me@justingreenberg.com>
2016-08-01 17:24:43 -0400

    chore(eslint): add missing eslint-disable on HomePage

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -35,7 +35,7 @@ import LoadingIndicator from 'components/LoadingIndicator';
 
 import styles from './styles.css';
 
-export class HomePage extends React.PureComponent {
+export class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   /**
    * when initial state username is not null, submit the form to load repos
    */
