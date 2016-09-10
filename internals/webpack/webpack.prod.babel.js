16cdb9067aefd2f19c6a088b948916828084dd92
Glen Ihrig <glen@arkadias.com>
2016-09-10 05:57:10 -0700

    fix(#950) WEBPACK_IMPORTED_MODULE_3_redux_saga... (#952)
    * fix(#950) WIP several steps toward solution - not there yet
    
    See https://github.com/mxstbr/react-boilerplate/issues/950
    
    * chore(webpack): fallbackloader -> fallbackLoader

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -24,10 +24,10 @@ module.exports = require('./webpack.base.babel')({
 
   // We use ExtractTextPlugin so we get a seperate CSS file instead
   // of the CSS being in the JS and injected as a style tag
-  cssLoaders: ExtractTextPlugin.extract(
-    'style-loader',
-    'css-loader?modules&-autoprefixer&importLoaders=1!postcss-loader'
-  ),
+  cssLoaders: ExtractTextPlugin.extract({
+    fallbackLoader: 'style-loader',
+    loader: 'css-loader?modules&-autoprefixer&importLoaders=1!postcss-loader',
+  }),
 
   // In production, we minify our CSS with cssnano
   postcssPlugins: [
