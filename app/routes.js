720806affb4a9f3e5ca38372801a8d2995ac144d
Hendrik Swanepoel <hendrik.swanepoel@gmail.com>
2016-09-17 14:05:16 +0200

    default code uses singleton saga

--- app/routes.js
+++ app/routes.js
@@ -31,7 +31,7 @@ export default function createRoutes(store) {
 
         importModules.then(([reducer, sagas, component]) => {
           injectReducer('home', reducer.default);
-          injectSagas(sagas.default);
+          injectSagas('home', sagas.default);
 
           renderRoute(component);
         });
