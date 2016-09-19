dcad6e0c0baccb333a9881313f5cb2ec4b9009c5
ansumanshah <ansumanshah@gmail.com>
2016-09-19 22:47:12 +0530

    Removed devtools from production

--- app/store.js
+++ app/store.js
@@ -27,6 +27,7 @@ export default function configureStore(initialState = {}, history) {
   /* eslint-disable no-underscore-dangle */
   const composeEnhancers =
     process.env.NODE_ENV !== 'production' &&
+    typeof window === 'object' &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
       window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
   /* eslint-enable */
