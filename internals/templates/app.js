865da0fd7244362fe499f019fdd1d1325fe49cf3
Amila Welihinda <amilajack@users.noreply.github.com>
2016-10-02 23:11:31 -0700

    ESLint config airbnb v12 migration (#1067)

--- internals/templates/app.js
+++ internals/templates/app.js
@@ -6,11 +6,11 @@
  */
 import 'babel-polyfill';
 
-/* eslint-disable import/no-unresolved */
+/* eslint-disable import/no-unresolved, import/extensions */
 // Load the manifest.json file and the .htaccess file
 import '!file?name=[name].[ext]!./manifest.json';
 import 'file?name=[name].[ext]!./.htaccess';
-/* eslint-enable import/no-unresolved */
+/* eslint-enable import/no-unresolved, import/extensions */
 
 // Import all the third party stuff
 import React from 'react';
