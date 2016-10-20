f8f2e5c6c2e680e63c408ee3664f2fdc8cd12cf7
Quentin Rousseau <quentin@instacart.com>
2016-10-20 13:29:40 -0700

    Support socket connection

--- server/logger.js
+++ server/logger.js
@@ -16,7 +16,7 @@ const logger = {
   },
 
   // Called when express.js app starts on given port w/o errors
-  appStarted: (port, tunnelStarted) => {
+  appStarted: (port_or_socket, tunnelStarted) => {
     console.log(`Server started ${chalk.green('✓')}`);
 
     // If the tunnel started, log that and the URL it's available at
@@ -24,13 +24,21 @@ const logger = {
       console.log(`Tunnel initialised ${chalk.green('✓')}`);
     }
 
-    console.log(`
-${chalk.bold('Access URLs:')}${divider}
-Localhost: ${chalk.magenta(`http://localhost:${port}`)}
-      LAN: ${chalk.magenta(`http://${ip.address()}:${port}`) +
-(tunnelStarted ? `\n    Proxy: ${chalk.magenta(tunnelStarted)}` : '')}${divider}
-${chalk.blue(`Press ${chalk.italic('CTRL-C')} to stop`)}
-    `);
+    if (typeof(port_or_socket) === 'string') {
+      console.log(`
+  ${chalk.bold('Server listening on socket:')}${divider}
+  Socket: ${chalk.magenta(port_or_socket)}${divider}
+  ${chalk.blue(`Press ${chalk.italic('CTRL-C')} to stop`)}
+      `);
+    } else {
+      console.log(`
+  ${chalk.bold('Access URLs:')}${divider}
+  Localhost: ${chalk.magenta(`http://localhost:${port_or_socket}`)}
+        LAN: ${chalk.magenta(`http://${ip.address()}:${port_or_socket}`) +
+  (tunnelStarted ? `\n    Proxy: ${chalk.magenta(tunnelStarted)}` : '')}${divider}
+  ${chalk.blue(`Press ${chalk.italic('CTRL-C')} to stop`)}
+      `);
+    }
   },
 };
 
