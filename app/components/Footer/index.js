2f987d144d94a18eb08cf7bdbad8495f00a21624
Sébastien Dubois <sedubois@users.noreply.github.com>
2016-11-02 15:10:03 +0200

    feat(example): Simplify footer CSS (#1076)

--- app/components/Footer/index.js
+++ app/components/Footer/index.js
@@ -10,22 +10,18 @@ function Footer() {
   return (
     <Wrapper>
       <section>
-        <p>
-          <FormattedMessage {...messages.licenseMessage} />
-        </p>
+        <FormattedMessage {...messages.licenseMessage} />
       </section>
       <section>
         <LocaleToggle />
       </section>
       <section>
-        <p>
-          <FormattedMessage
-            {...messages.authorMessage}
-            values={{
-              author: <A href="https://twitter.com/mxstbr">Max Stoiber</A>,
-            }}
-          />
-        </p>
+        <FormattedMessage
+          {...messages.authorMessage}
+          values={{
+            author: <A href="https://twitter.com/mxstbr">Max Stoiber</A>,
+          }}
+        />
       </section>
     </Wrapper>
   );
