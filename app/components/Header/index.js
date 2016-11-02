56f3f68f5d1170a084e081a9525842aca63784b6
Sébastien Dubois <sedubois@users.noreply.github.com>
2016-11-02 16:08:56 +0200

    feat(example): Simplify navigation, extract to header (#1079)

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import { FormattedMessage } from 'react-intl';
 
 import A from './A';
 import Img from './Img';
+import NavBar from './NavBar';
+import HeaderLink from './HeaderLink';
 import Banner from './banner.jpg';
+import messages from './messages';
 
 class Header extends React.Component { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <A href="https://twitter.com/mxstbr">
-        <Img src={Banner} alt="react-boilerplate - Logo" />
-      </A>
+      <div>
+        <A href="https://twitter.com/mxstbr">
+          <Img src={Banner} alt="react-boilerplate - Logo" />
+        </A>
+        <NavBar>
+          <HeaderLink to="/">
+            <FormattedMessage {...messages.home} />
+          </HeaderLink>
+          <HeaderLink to="/features">
+            <FormattedMessage {...messages.features} />
+          </HeaderLink>
+        </NavBar>
+      </div>
     );
   }
 }
--- app/containers/NotFoundPage/index.js
+++ app/containers/NotFoundPage/index.js
@@ -5,34 +5,17 @@
  */
 
 import React from 'react';
-import { connect } from 'react-redux';
-import { push } from 'react-router-redux';
 
 import messages from './messages';
 import { FormattedMessage } from 'react-intl';
-import Button from 'components/Button';
 import H1 from 'components/H1';
 
-export function NotFound(props) {
+export default function NotFound() {
   return (
     <article>
       <H1>
         <FormattedMessage {...messages.header} />
       </H1>
-      <Button
-        handleRoute={function redirect() {
-          props.dispatch(push('/'));
-        }}
-      >
-        <FormattedMessage {...messages.homeButton} />
-      </Button>
     </article>
   );
 }
-
-NotFound.propTypes = {
-  dispatch: React.PropTypes.func,
-};
-
-// Wrap the component to inject dispatch and state into it
-export default connect()(NotFound);
