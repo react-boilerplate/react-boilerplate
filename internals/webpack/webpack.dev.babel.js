5e13b8c047f7b7a8f969a9b947c8fa28665b093b
Marnus Weststrate <marnusw@gmail.com>
2016-11-07 10:19:47 +0000

    Switch out hmre preset for react-hot-loader v3.

--- internals/webpack/webpack.dev.babel.js
+++ internals/webpack/webpack.dev.babel.js
@@ -24,6 +24,7 @@ module.exports = require('./webpack.base.babel')({
   // Add hot reloading in development
   entry: [
     'eventsource-polyfill', // Necessary for hot reloading with IE
+    'react-hot-loader/patch',
     'webpack-hot-middleware/client',
     path.join(process.cwd(), 'app/app.js'), // Start with js/app.js
   ],
@@ -39,11 +40,7 @@ module.exports = require('./webpack.base.babel')({
 
   // Tell babel that we want to hot-reload
   babelQuery: {
-    // require.resolve solves the issue of relative presets when dealing with
-    // locally linked packages. This is an issue with babel and webpack.
-    // See https://github.com/babel/babel-loader/issues/149 and
-    // https://github.com/webpack/webpack/issues/1866
-    presets: ['babel-preset-react-hmre'].map(require.resolve),
+    plugins: ['react-hot-loader/babel'],
   },
 
   // Emit a source map for easier debugging
