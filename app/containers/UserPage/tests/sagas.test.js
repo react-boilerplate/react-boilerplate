c814d24f4ae839383ff7f23362533ddd9057c24b
tomazy <tomazy@go2.pl>
2016-11-16 18:46:43 +0100

    Added saga tests

---
+++ app/containers/UserPage/tests/sagas.test.js
@@ -0,0 +1,69 @@
+/**
+ * Tests for HomePage sagas
+ */
+
+import { takeLatest } from 'redux-saga';
+import { take, put, fork } from 'redux-saga/effects';
+
+import { LOCATION_CHANGE } from 'react-router-redux';
+
+import { getRepos, getReposWatcher, githubData } from '../sagas';
+
+import { LOAD_REPOS } from 'containers/App/constants';
+import { reposLoaded, repoLoadingError } from 'containers/App/actions';
+
+const username = 'mxstbr';
+
+describe('getRepos Saga', () => {
+  let getReposGenerator;
+
+  // We have to test twice, once for a successful load and once for an unsuccessful one
+  // so we do all the stuff that happens beforehand automatically in the beforeEach
+  beforeEach(() => {
+    getReposGenerator = getRepos({ username });
+
+    const callDescriptor = getReposGenerator.next(username).value;
+    expect(callDescriptor).toMatchSnapshot();
+  });
+
+  it('should dispatch the reposLoaded action if it requests the data successfully', () => {
+    const response = [{
+      name: 'First repo',
+    }, {
+      name: 'Second repo',
+    }];
+    const putDescriptor = getReposGenerator.next(response).value;
+    expect(putDescriptor).toEqual(put(reposLoaded(response, username)));
+  });
+
+  it('should call the repoLoadingError action if the response errors', () => {
+    const response = new Error('Some error');
+    const putDescriptor = getReposGenerator.throw(response).value;
+    expect(putDescriptor).toEqual(put(repoLoadingError(response)));
+  });
+});
+
+describe('getReposWatcher Saga', () => {
+  const getReposWatcherGenerator = getReposWatcher();
+
+  it('should watch for LOAD_REPOS action', () => {
+    const takeDescriptor = getReposWatcherGenerator.next().value;
+    expect(takeDescriptor).toEqual(fork(takeLatest, LOAD_REPOS, getRepos));
+  });
+});
+
+describe('githubDataSaga Saga', () => {
+  const githubDataSaga = githubData();
+
+  let forkDescriptor;
+
+  it('should asyncronously fork getReposWatcher saga', () => {
+    forkDescriptor = githubDataSaga.next();
+    expect(forkDescriptor.value).toEqual(fork(getReposWatcher));
+  });
+
+  it('should yield until LOCATION_CHANGE action', () => {
+    const takeDescriptor = githubDataSaga.next();
+    expect(takeDescriptor.value).toEqual(take(LOCATION_CHANGE));
+  });
+});
