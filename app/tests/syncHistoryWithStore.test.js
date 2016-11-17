22c7de183f100a62692e11065f18bddf3ebf66fe
tomazy <tomazy@go2.pl>
2016-11-17 16:11:30 +0100

    Added test for sync history module

---
+++ app/tests/syncHistoryWithStore.test.js
@@ -0,0 +1,14 @@
+jest.mock('react-router-redux');
+
+import { syncHistoryWithStore } from 'react-router-redux';
+
+import syncHistory from '../syncHistoryWithStore';
+
+describe('configureStore', () => {
+  it('should synchronise the history with the store', () => {
+    const history = {};
+    const store = {};
+    syncHistory(history, store);
+    expect(syncHistoryWithStore).toHaveBeenCalled();
+  });
+});
