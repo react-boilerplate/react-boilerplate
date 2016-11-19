c80dc21fb7c81708bc03ec26e26929babb479e91
tomazy <tomazy@go2.pl>
2016-11-19 12:25:57 +0100

    Updated app.js in templates

--- internals/templates/app.js
+++ internals/templates/app.js
@@ -4,101 +4,64 @@
  * This is the entry file for the application, only setup and boilerplate
  * code.
  */
+
+// Needed for redux-saga es6 generator support
 import 'babel-polyfill';
 
 /* eslint-disable import/no-unresolved, import/extensions */
-// Load the manifest.json file and the .htaccess file
+// Load the favicon, the manifest.json file and the .htaccess file
+import 'file?name=[name].[ext]!./favicon.ico';
 import '!file?name=[name].[ext]!./manifest.json';
 import 'file?name=[name].[ext]!./.htaccess';
 /* eslint-enable import/no-unresolved, import/extensions */
 
-// Import all the third party stuff
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { applyRouterMiddleware, Router, browserHistory } from 'react-router';
-import { syncHistoryWithStore } from 'react-router-redux';
-import { useScroll } from 'react-router-scroll';
-import LanguageProvider from 'containers/LanguageProvider';
-import configureStore from './store';
-
-// Import i18n messages
-import { translationMessages } from './i18n';
-
-// Import the CSS reset, which HtmlWebpackPlugin transfers to the build folder
+// Import CSS reset
 import 'sanitize.css/sanitize.css';
+
+import syncHistoryWithStore from 'setup/syncHistoryWithStore';
+import ensureIntlSupport from 'setup/ensureIntlSupport';
+import 'setup/openSansObserver';
+
+// Import all the third party stuff
+import { browserHistory } from 'react-router';
+import configureStore from './store';
+import renderInBrowser from './renderInBrowser';
+import createRoutes from './routes';
+import { translationMessages as messages } from './i18n';
+
+// The initial state of the app can be set on the server
+const initialState = window.APP_STATE || {};
 
 // Create redux store with history
 // this uses the singleton browserHistory provided by react-router
 // Optionally, this could be changed to leverage a created history
 // e.g. `const browserHistory = useRouterHistory(createBrowserHistory)();`
-const initialState = {};
 const store = configureStore(initialState, browserHistory);
+
+const routes = createRoutes(store);
 
 // Sync history and store, as the react-router-redux reducer
 // is under the non-default key ("routing"), selectLocationState
 // must be provided for resolving how to retrieve the "route" in the state
-import { selectLocationState } from 'containers/App/selectors';
-const history = syncHistoryWithStore(browserHistory, store, {
-  selectLocationState: selectLocationState(),
-});
+const history = syncHistoryWithStore(browserHistory, store);
 
-// Set up the router, wrapping all Routes in the App component
-import App from 'containers/App';
-import createRoutes from './routes';
-const rootRoute = {
-  component: App,
-  childRoutes: createRoutes(store),
-};
+function render() {
+  renderInBrowser({ messages, store, routes, history });
+}
 
-
-const render = (translatedMessages) => {
-  ReactDOM.render(
-    <Provider store={store}>
-      <LanguageProvider messages={translatedMessages}>
-        <Router
-          history={history}
-          routes={rootRoute}
-          render={
-            // Scroll to top when going to a new page, imitating default browser
-            // behaviour
-            applyRouterMiddleware(useScroll())
-          }
-        />
-      </LanguageProvider>
-    </Provider>,
-    document.getElementById('app')
-  );
-};
-
+ensureIntlSupport()
+  .then(render);
 
 // Hot reloadable translation json files
 if (module.hot) {
   // modules.hot.accept does not accept dynamic dependencies,
   // have to be constants at compile-time
-  module.hot.accept('./i18n', () => {
-    render(translationMessages);
-  });
-}
-
-// Chunked polyfill for browsers without Intl support
-if (!window.Intl) {
-  (new Promise((resolve) => {
-    resolve(System.import('intl'));
-  }))
-    .then(() => Promise.all([
-      System.import('intl/locale-data/jsonp/en.js'),
-    ]))
-    .then(() => render(translationMessages))
-    .catch((err) => {
-      throw err;
-    });
-} else {
-  render(translationMessages);
+  module.hot.accept('./i18n', render);
 }
 
 // Install ServiceWorker and AppCache in the end since
 // it's not most important operation and if main code fails,
 // we do not want it installed
-import { install } from 'offline-plugin/runtime';
-install();
+if (process.env.NODE_ENV === 'production') {
+  require('offline-plugin/runtime').install(); // eslint-disable-line global-require
+}
