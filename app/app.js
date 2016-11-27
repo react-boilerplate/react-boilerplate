9525c9c6fda6bdab3c1bc345b0ade44f43a76036
Mihir Karandikar <karandikar.mihir@outlook.com>
2016-11-27 17:57:19 +0530

    Revert favicon, manifest, .htaccess imports

--- app/app.js
+++ app/app.js
@@ -21,10 +21,11 @@ import 'sanitize.css/sanitize.css';
 import configureStore from './store';
 
 // Load the favicon, the manifest.json file and the .htaccess file
-import './favicon.ico';
-
-import '!file?name=[name].[ext]!./manifest.json'; // eslint-disable-line import/no-webpack-loader-syntax
-import 'file?name=[name].[ext]!./.htaccess'; // eslint-disable-line import/extensions, import/no-webpack-loader-syntax
+/* eslint-disable-line import/no-webpack-loader-syntax */
+import '!file?name=[name].[ext]!./favicon.ico';
+import '!file?name=[name].[ext]!./manifest.json';
+import 'file?name=[name].[ext]!./.htaccess'; // eslint-disable-line import/extensions
+/* eslint-enable import/no-webpack-loader-syntax */
 
 // Import Language Provider
 import LanguageProvider from 'containers/LanguageProvider';
