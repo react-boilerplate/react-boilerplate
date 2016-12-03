7416d9be6c9415158d0447de0f44e7d9cdd7c029
Mihir Karandikar <karandikar.mihir@outlook.com>
2016-12-03 13:37:25 +0530

    Move offline-plugin import to the top in templates/app.js

--- internals/templates/app.js
+++ internals/templates/app.js
@@ -14,6 +14,11 @@ import { applyRouterMiddleware, Router, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { useScroll } from 'react-router-scroll';
 
+// Install ServiceWorker and AppCache in the end since
+// it's not most important operation and if main code fails,
+// we do not want it installed
+import { install } from 'offline-plugin/runtime';
+
 // Import the CSS reset, which HtmlWebpackPlugin transfers to the build folder
 import 'sanitize.css/sanitize.css';
 
@@ -101,8 +106,4 @@ if (!window.Intl) {
   render(translationMessages);
 }
 
-// Install ServiceWorker and AppCache in the end since
-// it's not most important operation and if main code fails,
-// we do not want it installed
-import { install } from 'offline-plugin/runtime';
 install();
