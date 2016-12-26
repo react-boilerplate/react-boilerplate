6cd3e76e83da6bf930379c0f3ee75548e1372b54
Max <contact@mstoiber.com>
2016-12-26 11:46:17 +0100

    Split ProgressBar/styles.js into two separate files

--- app/components/ProgressBar/Wrapper.js
+++ app/components/ProgressBar/Wrapper.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
-/* eslint-disable */
-export const ProgressBarWrapper = styled.div`
+export default styled.div`
   position: fixed;
   top: 0;
   left: 0;
@@ -10,11 +9,4 @@ export const ProgressBarWrapper = styled.div`
   opacity: ${(props) => props.hidden ? '0' : '1'};
   transition: all 500ms ease-in-out;
   z-index: ${(props) => props.hidden ? '-10' : '9999'};
-`;
-/* eslint-enable */
-
-export const ProgressBarPercent = styled.div`
-  height: 2px;
-  background: #29D;
-  transition: all 300ms ease;
 `;
