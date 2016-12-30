1e0f1c0a8852f4de624194a095c9d6041878949e
Johnathon Sanders <johnathon.sanders@target.com>
2016-12-30 11:27:16 -0600

    Update templates and generators
    - Take the next step and bring the default containers to Pure Functions.
    - Also, align import formatting separation with the work done in #1275.

--- internals/templates/languageProvider/languageProvider.js
+++ internals/templates/languageProvider/languageProvider.js
@@ -13,14 +13,12 @@ import { IntlProvider } from 'react-intl';
 
 import { makeSelectLocale } from './selectors';
 
-export class LanguageProvider extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
-  render() {
-    return (
-      <IntlProvider locale={this.props.locale} key={this.props.locale} messages={this.props.messages[this.props.locale]}>
-        {React.Children.only(this.props.children)}
-      </IntlProvider>
-    );
-  }
+export function LanguageProvider({ locale, messages, children }) {
+  return (
+    <IntlProvider locale={locale} key={locale} messages={messages[locale]}>
+      {React.Children.only(children)}
+    </IntlProvider>
+  );
 }
 
 LanguageProvider.propTypes = {
--- internals/templates/notFoundPage/notFoundPage.js
+++ internals/templates/notFoundPage/notFoundPage.js
@@ -2,11 +2,6 @@
  * NotFoundPage
  *
  * This is the page we show when the user visits a url that doesn't have a route
- *
- * NOTE: while this component should technically be a stateless functional
- * component (SFC), hot reloading does not currently support SFCs. If hot
- * reloading is not a necessity for you then you can refactor it and remove
- * the linting exception.
  */
 
 import React from 'react';
@@ -14,12 +9,12 @@ import { FormattedMessage } from 'react-intl';
 
 import messages from './messages';
 
-export default class NotFound extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
-  render() {
-    return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
-    );
-  }
+function NotFound() {
+  return (
+    <h1>
+      <FormattedMessage {...messages.header} />
+    </h1>
+  );
 }
+
+export default NotFound;
