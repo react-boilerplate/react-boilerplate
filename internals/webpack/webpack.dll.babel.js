2b6fbaa9d33a7c9c00ea44af394a53f704cc9c61
Yaroslav Kiliba <om.dattaya@gmail.com>
2017-01-07 21:40:33 +0200

    Fix warning during generation of webpack dll file

--- internals/webpack/webpack.dll.babel.js
+++ internals/webpack/webpack.dll.babel.js
@@ -31,4 +31,7 @@ module.exports = require('./webpack.base.babel')({
   plugins: [
     new webpack.DllPlugin({ name: '[name]', path: join(outputPath, '[name].json') }), // eslint-disable-line no-new
   ],
+  performance: {
+    hints: false,
+  },
 });
