c15f1a9d94ab9a3a1b458400b87b08040967df15
Johnathon Sanders <johnathon.sanders@target.com>
2017-01-07 11:31:56 -0600

    Move to container structure for internals

--- internals/scripts/clean.js
+++ internals/scripts/clean.js
@@ -13,67 +13,36 @@ if (!test('-e', 'internals/templates')) {
 
 process.stdout.write('Cleanup started...');
 
-// Cleanup components folder
+// Reuse existing LanguageProvider and i18n tests
+mv('app/containers/LanguageProvider/tests', 'internals/templates/containers/LanguageProvider');
+cp('app/tests/i18n.test.js', 'internals/templates/tests/i18n.test.js');
+
+// Cleanup components/
 rm('-rf', 'app/components/*');
 
-// Save LanguageProvider tests
-mv('app/containers/LanguageProvider/tests', 'internals/templates/languageProvider/');
-
-// Cleanup containers folder
+// Handle containers/
 rm('-rf', 'app/containers/*');
-mkdir('-p', 'app/containers/App/tests');
-mkdir('-p', 'app/containers/NotFoundPage/tests');
-mkdir('-p', 'app/containers/HomePage/tests');
+mv('internals/templates/containers', 'app');
 
-cp('internals/templates/appContainer/index.js', 'app/containers/App/index.js');
-cp('internals/templates/appContainer/tests/index.test.js', 'app/containers/App/tests/index.test.js');
-cp('internals/templates/appContainer/constants.js', 'app/containers/App/constants.js');
-cp('internals/templates/appContainer/selectors.js', 'app/containers/App/selectors.js');
-cp('internals/templates/appContainer/tests/selectors.test.js', 'app/containers/App/tests/selectors.test.js');
+// Handle tests/
+mv('internals/templates/tests', 'app');
 
-cp('internals/templates/notFoundPage/index.js', 'app/containers/NotFoundPage/index.js');
-cp('internals/templates/notFoundPage/messages.js', 'app/containers/NotFoundPage/messages.js');
-cp('internals/templates/notFoundPage/tests/index.test.js', 'app/containers/NotFoundPage/tests/index.test.js');
-
-cp('internals/templates/homePage/index.js', 'app/containers/HomePage/index.js');
-cp('internals/templates/homePage/messages.js', 'app/containers/HomePage/messages.js');
-cp('internals/templates/homePage/tests/index.test.js', 'app/containers/HomePage/tests/index.test.js');
-
-// Handle Translations
+// Handle translations/
 rm('-rf', 'app/translations/*')
-mkdir('-p', 'app/translations');
-cp('internals/templates/translations/en.json',
-  'app/translations/en.json');
+mv('internals/templates/translations', 'app');
 
-// move i18n file
-cp('internals/templates/i18n.js',
-  'app/i18n.js');
-
-// Copy LanguageProvider
-mkdir('-p', 'app/containers/LanguageProvider');
-mv('internals/templates/languageProvider/tests', 'app/containers/LanguageProvider');
-cp('internals/templates/languageProvider/actions.js',
-  'app/containers/LanguageProvider/actions.js');
-cp('internals/templates/languageProvider/constants.js',
-  'app/containers/LanguageProvider/constants.js');
-cp('internals/templates/languageProvider/languageProvider.js',
-  'app/containers/LanguageProvider/index.js');
-cp('internals/templates/languageProvider/reducer.js',
-  'app/containers/LanguageProvider/reducer.js');
-cp('internals/templates/languageProvider/selectors.js',
-  'app/containers/LanguageProvider/selectors.js');
-
-// Utils
+// Handle utils/
 rm('-rf', 'app/utils');
 mv('internals/templates/utils', 'app')
 
 // Replace the files in the root app/ folder
 cp('internals/templates/app.js', 'app/app.js');
+cp('internals/templates/global-styles.js', 'app/global-styles.js');
+cp('internals/templates/i18n.js', 'app/i18n.js');
 cp('internals/templates/index.html', 'app/index.html');
 cp('internals/templates/reducers.js', 'app/reducers.js');
 cp('internals/templates/routes.js', 'app/routes.js');
 cp('internals/templates/store.js', 'app/store.js');
-cp('internals/templates/appTests/store.test.js', 'app/tests/store.test.js');
 
 // Remove the templates folder
 rm('-rf', 'internals/templates');
