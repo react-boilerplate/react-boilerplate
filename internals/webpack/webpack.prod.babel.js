32a672918005ee8e8e1a554ed4f493895329858f
Yaroslav Kiliba <om.dattaya@gmail.com>
2017-01-09 21:52:38 +0200

    chore(internals): do not check file sizes of main chunk and favicon

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -67,4 +67,8 @@ module.exports = require('./webpack.base.babel')({
       AppCache: false,
     }),
   ],
+
+  performance: {
+    assetFilter: (assetFilename) => !(/(\.map$)|(^(main\.|favicon\.))/.test(assetFilename)),
+  },
 });
