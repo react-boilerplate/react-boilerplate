c471b1e81f8885f378e18a49a9b61aaa68726bff
Yaroslav Kiliba <om.dattaya@gmail.com>
2017-01-11 21:19:51 +0200

    chore(deps): remove chai and chai-enzyme

--- internals/testing/test-bundler.js
+++ internals/testing/test-bundler.js
@@ -1,8 +1,3 @@
 // needed for regenerator-runtime
 // (ES7 generator support is required by redux-saga)
 import 'babel-polyfill';
-
-// If we need to use Chai, we'll have already chaiEnzyme loaded
-import chai from 'chai';
-import chaiEnzyme from 'chai-enzyme';
-chai.use(chaiEnzyme());
