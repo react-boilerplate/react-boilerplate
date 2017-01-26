555ccf3ff784d89f6de1920fc55386513c0a76b8
Necmttn <necmettin.karakaya@gmail.com>
2017-01-26 11:19:24 +0800

    Generator Warnings (#1462)
    Added warning promt for warn user about add reducer to their
    reducers.js

--- internals/generators/container/index.js
+++ internals/generators/container/index.js
@@ -3,6 +3,7 @@
  */
 
 const componentExists = require('../utils/componentExists');
+const chalk = require('chalk');
 
 module.exports = {
   description: 'Add a container component',
@@ -34,6 +35,14 @@ module.exports = {
     name: 'wantActionsAndReducer',
     default: true,
     message: 'Do you want an actions/constants/selectors/reducer tuple for this container?',
+  }, {
+    when: function (response) {
+      return response.wantActionsAndReducer
+    },
+    name: 'actionsAndReducerWarning',
+    type: 'confirm',
+    default: false,
+    message: chalk.yellow('️ ️⚠️️ ️You need to combine your reducer after you create this component/container!')
   }, {
     type: 'confirm',
     name: 'wantSagas',
