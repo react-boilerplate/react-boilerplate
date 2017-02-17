34e05f720fdefb156b63f023663eca6dda952072
chaintng <chaintng@gmail.com>
2017-02-17 23:53:29 +0700

    fix test case

--- app/components/List/tests/index.test.js
+++ app/components/List/tests/index.test.js
@@ -12,16 +12,19 @@ describe('<List />', () => {
     expect(renderedComponent.find(ListItem)).toBeDefined();
   });
 
-  it('should render the items', () => {
+  it('should pass all items props to rendered component', () => {
     const items = [
       { id: 1, name: 'Hello' },
       { id: 2, name: 'World' },
     ];
 
+    const component = ({ item }) => <ListItem>{item.name}</ListItem>; // eslint-disable-line react/prop-types
+
     const renderedComponent = shallow(
-      <List items={items} component={ListItem} />
+      <List items={items} component={component} />
     );
-    expect(renderedComponent.find(ListItem)).toBeDefined();
-    expect(renderedComponent.find(ListItem).length).toEqual(2);
+    expect(renderedComponent.find(component)).toHaveLength(2);
+    expect(renderedComponent.find(component).at(0).prop('item')).toBe(items[0]);
+    expect(renderedComponent.find(component).at(1).prop('item')).toBe(items[1]);
   });
 });
