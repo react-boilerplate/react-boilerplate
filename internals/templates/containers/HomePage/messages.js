7ee037f308dfb8821ef57ac74dde1a6a4471d837
Aneesh Karve <aneesh.karve@gmail.com>
2017-02-19 11:43:20 -0500

    revise text per PR feedback

--- internals/templates/containers/HomePage/messages.js
+++ internals/templates/containers/HomePage/messages.js
@@ -8,6 +8,6 @@ import { defineMessages } from 'react-intl';
 export default defineMessages({
   header: {
     id: 'app.components.HomePage.header',
-    defaultMessage: 'This is the HomePage container (a React component)!',
+    defaultMessage: 'This is the HomePage container!',
   },
 });
