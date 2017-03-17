b99da8eaab65594087925b1fbc6b40e1f3576676
collectivelymad <PPowers78>
2017-03-17 16:14:54 -0700

    link cleanup

--- app/containers/ContactUsPage/index.js
+++ app/containers/ContactUsPage/index.js
@@ -5,7 +5,7 @@
  */
 import React from 'react';
 import Helmet from 'react-helmet';
-import {FormattedMessage} from 'react-intl';
+import { FormattedMessage } from 'react-intl';
 
 import H1 from 'components/H1';
 import messages from './messages';
@@ -25,7 +25,7 @@ export default class ContactUsPage extends React.Component { // eslint-disable-l
         <Helmet
           title="Contact Us"
           meta={[
-            {name: 'description', content: 'Never Pay More'},
+            { name: 'description', content: 'Never Pay More' },
           ]}
         />
         <H1>
