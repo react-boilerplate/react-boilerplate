e79d43b83df434c32a4493b495775dda9da1790e
abdulaleem313 <abdulaleem313@yahoo.com>
2017-05-30 15:01:53 +0500

    react table on feature page with test data

--- app/containers/FeaturePage/index.js
+++ app/containers/FeaturePage/index.js
@@ -7,12 +7,15 @@ import React from 'react';
 import Helmet from 'react-helmet';
 import { FormattedMessage } from 'react-intl';
 
+import ReactTable from 'react-table'
+
 import H1 from 'components/H1';
 import messages from './messages';
 import List from './List';
 import ListItem from './ListItem';
 import ListItemTitle from './ListItemTitle';
 
+import 'react-table/react-table.css'
 export default class FeaturePage extends React.Component { // eslint-disable-line react/prefer-stateless-function
 
   // Since state and props are static,
@@ -22,6 +25,202 @@ export default class FeaturePage extends React.Component { // eslint-disable-lin
   }
 
   render() {
+
+      const data = [{
+        name: 'Tanner Linsley',
+        age: 26,
+        friend: {
+          name: 'Jason Maurer',
+          age: 23,
+        }
+      },{
+        name: 'Tanner Linsley',
+        age: 26,
+        friend: {
+          name: 'Jason Maurer',
+          age: 23,
+        }
+      },{
+        name: 'Tanner Linsley',
+        age: 26,
+        friend: {
+          name: 'Jason Maurer',
+          age: 23,
+        }
+      },{
+        name: 'Tanner Linsley',
+        age: 26,
+        friend: {
+          name: 'Jason Maurer',
+          age: 23,
+        }
+      },{
+        name: 'Tanner Linsley',
+        age: 26,
+        friend: {
+          name: 'Jason Maurer',
+          age: 23,
+        }
+      },{
+        name: 'Tanner Linsley',
+        age: 26,
+        friend: {
+          name: 'Jason Maurer',
+          age: 23,
+        }
+      },{
+        name: 'Tanner Linsley',
+        age: 26,
+        friend: {
+          name: 'Jason Maurer',
+          age: 23,
+        }
+      },{
+        name: 'Tanner Linsley',
+        age: 26,
+        friend: {
+          name: 'Jason Maurer',
+          age: 23,
+        }
+      },{
+        name: 'Tanner Linsley',
+        age: 26,
+        friend: {
+          name: 'Jason Maurer',
+          age: 23,
+        }
+      },{
+        name: 'Tanner Linsley',
+        age: 26,
+        friend: {
+          name: 'Jason Maurer',
+          age: 23,
+        }
+      },{
+        name: 'Tanner Linsley',
+        age: 26,
+        friend: {
+          name: 'Jason Maurer',
+          age: 23,
+        }
+      },{
+        name: 'Tanner Linsley',
+        age: 26,
+        friend: {
+          name: 'Jason Maurer',
+          age: 23,
+        }
+      },{
+        name: 'Tanner Linsley',
+        age: 26,
+        friend: {
+          name: 'Jason Maurer',
+          age: 23,
+        }
+      },{
+        name: 'Tanner Linsley',
+        age: 26,
+        friend: {
+          name: 'Jason Maurer',
+          age: 23,
+        }
+      },{
+        name: 'Tanner Linsley',
+        age: 26,
+        friend: {
+          name: 'Jason Maurer',
+          age: 23,
+        }
+      },{
+        name: 'Tanner Linsley',
+        age: 26,
+        friend: {
+          name: 'Jason Maurer',
+          age: 23,
+        }
+      },{
+        name: 'Tanner Linsley',
+        age: 26,
+        friend: {
+          name: 'Jason Maurer',
+          age: 23,
+        }
+      },{
+        name: 'Tanner Linsley',
+        age: 26,
+        friend: {
+          name: 'Jason Maurer',
+          age: 23,
+        }
+      },{
+        name: 'Tanner Linsley',
+        age: 26,
+        friend: {
+          name: 'Jason Maurer',
+          age: 23,
+        }
+      },{
+        name: 'Tanner Linsley',
+        age: 26,
+        friend: {
+          name: 'Jason Maurer',
+          age: 23,
+        }
+      },{
+        name: 'Tanner Linsley',
+        age: 26,
+        friend: {
+          name: 'Jason Maurer',
+          age: 23,
+        }
+      },{
+        name: 'Tanner Linsley',
+        age: 26,
+        friend: {
+          name: 'Jason Maurer',
+          age: 23,
+        }
+      },{
+        name: 'Tanner 34 Linsley',
+        age: 26,
+        friend: {
+          name: 'Jason Maurer',
+          age: 23,
+        }
+      },{
+        name: 'Tannerwer Linsley',
+        age: 26,
+        friend: {
+          name: 'Jason Maurer',
+          age: 23,
+        }
+      },{
+        name: 'Tannerasdfas Linsley',
+        age: 26,
+        friend: {
+          name: 'Jason Maurer',
+          age: 23,
+        }
+      }]
+
+  const columns = [{
+    Header: 'Name',
+    accessor: 'name' // String-based value accessors!
+  }, {
+    Header: 'Age',
+    accessor: 'age',
+    Cell: props => <span className='number'>{props.value}</span> // Custom cell components!
+  }, {
+    id: 'friendName', // Required because our accessor is not a string
+    Header: 'Friend Name',
+    accessor: d => d.friend.name // Custom value accessors!
+  }, {
+    Header: props => <span>Friend Age</span>, // Custom header components!
+    accessor: 'friend.age'
+  }]
+
+      
+
     return (
       <div>
         <Helmet
@@ -29,6 +228,10 @@ export default class FeaturePage extends React.Component { // eslint-disable-lin
           meta={[
             { name: 'description', content: 'Feature page of React.js Boilerplate application' },
           ]}
+        />
+        <ReactTable
+          data={data}
+          columns={columns}
         />
         <H1>
           <FormattedMessage {...messages.header} />
