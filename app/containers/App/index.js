d6862039bd3c4d0f032b682a6191a5bf985f7293
Yaroslav Kiliba <om.dattaya@gmail.com>
2017-06-17 12:05:30 +0300

    Rename `loader.js` to `Loadable.js`

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -11,8 +11,8 @@ import Helmet from 'react-helmet';
 import styled from 'styled-components';
 import { Switch, Route } from 'react-router-dom';
 
-import HomePage from 'containers/HomePage/Loader';
-import FeaturePage from 'containers/FeaturePage/Loader';
+import HomePage from 'containers/HomePage/Loadable';
+import FeaturePage from 'containers/FeaturePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage';
 import Header from 'components/Header';
 import Footer from 'components/Footer';
