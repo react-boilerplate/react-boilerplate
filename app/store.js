ce3e099021fe92703ef842637faf75ffaf9e1665
Yaroslav Kiliba <om.dattaya@gmail.com>
2017-07-23 13:44:13 +0300

    Prevent recomputing reducers for `replaceReducer`

--- app/store.js
+++ app/store.js
@@ -28,8 +28,13 @@ export default function configureStore(initialState = {}, history) {
   const composeEnhancers =
     process.env.NODE_ENV !== 'production' &&
     typeof window === 'object' &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-      window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ : compose;
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+      ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // TODO Try to remove when `react-router-redux` is out of beta, LOCATION_CHANGE should not be fired more than once after hot reloading
+        // Prevent recomputing reducers for `replaceReducer`
+        shouldHotReload: false,
+      })
+      : compose;
   /* eslint-enable */
 
   const store = createStore(
