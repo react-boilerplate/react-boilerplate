9cffa52849c7258ba3a6fc59fe3f69b583176322
Justin Greenberg <justingreenberg@users.noreply.github.com>
2017-10-04 13:39:27 -0400

    fix(webpack): Revert timeout query

--- internals/webpack/webpack.dev.babel.js
+++ internals/webpack/webpack.dev.babel.js
@@ -41,7 +41,7 @@ module.exports = require('./webpack.base.babel')({
   // Add hot reloading in development
   entry: [
     'eventsource-polyfill', // Necessary for hot reloading with IE
-    'webpack-hot-middleware/client?reload=true&timeout=2000',
+    'webpack-hot-middleware/client?reload=true',
     path.join(process.cwd(), 'app/app.js'), // Start with js/app.js
   ],
 
