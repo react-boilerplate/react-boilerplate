904c8a878279e3eedcbab59ed6934ac08c9eb610
Johnathon Sanders <outdooricon@gmail.com>
2017-12-14 20:10:13 -0600

    Fix ejection error of done sagas (#2040)
    * Fix ejection error of done sagas
    
    * Correct test for real scenario

--- app/utils/sagaInjectors.js
+++ app/utils/sagaInjectors.js
@@ -64,7 +64,7 @@ export function ejectSagaFactory(store, isValid) {
 
     if (Reflect.has(store.injectedSagas, key)) {
       const descriptor = store.injectedSagas[key];
-      if (descriptor.mode !== DAEMON) {
+      if (descriptor.mode && descriptor.mode !== DAEMON) {
         descriptor.task.cancel();
         // Clean up in production; in development we need `descriptor.saga` for hot reloading
         if (process.env.NODE_ENV === 'production') {
