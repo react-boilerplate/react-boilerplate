0cfd974b99dfd8c5a238e93890cf574832cad0c4
Josh Stabback <josh.stabback@gmail.com>
2018-01-31 06:27:29 -0500

    Scaffold styles constant

---
+++ internals/templates/styles/index.js
@@ -0,0 +1,31 @@
+export const breakpoint = {
+  small: '40em',
+  medium: '64em',
+  large: '75em',
+};
+
+export const color = {
+  primary: '#41addd',
+  primary_light: '#6cc0e5',
+  black: '#101010',
+  white: '#fafafa',
+  gray_1: '#999',
+  gray_2: '#666',
+
+};
+
+export const font = {
+  body: "Georgia, Times, 'Times New Roman', serif",
+  header: "'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif",
+};
+
+export const size = {
+  border_radius: '4px',
+};
+
+export default {
+  breakpoint,
+  color,
+  font,
+  size,
+};
