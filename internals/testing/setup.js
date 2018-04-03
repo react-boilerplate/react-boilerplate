e369f4e740158c17717b1b1a9522e30cd9b00160
gretzky <sfed617@gmail.com>
2018-04-03 12:46:02 -0400

    replace shim with raf polyfill

--- internals/testing/setup.js
+++ internals/testing/setup.js
@@ -1,5 +1,7 @@
 // Enzyme v3 requires use of adapters
 // https://github.com/airbnb/enzyme/blob/master/docs/guides/migration-from-2-to-3.md
+import 'raf/polyfill';
+
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 
--- internals/testing/shim.js
+++
@@ -1,5 +0,0 @@
-// shim that suppresses annoying jest warnings
-// https://github.com/facebook/jest/issues/4545#issuecomment-332762365
-global.requestAnimationFrame = (callback) => {
-  setTimeout(callback, 0);
-};
