5ed36bfe2d67daa3428398f9094c18b02de9d380
Shawn Stern <Shawn.Stern@nwea.org>
2018-05-02 13:16:36 -0700

    Updating order of module resolution in Webpack config, splitting onto multiple lines

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -125,7 +125,10 @@ module.exports = (options) => ({
     }),
   ]),
   resolve: {
-    modules: ['app', 'node_modules'],
+    modules: [
+      'node_modules',
+      'app',
+    ],
     extensions: [
       '.js',
       '.jsx',
