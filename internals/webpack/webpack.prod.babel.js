5fdf8e57ff1447f7303ffbc758dad6ca41214b8c
Johan Meester <walter.doodah@gmail.com>
2018-07-23 15:06:38 +0200

    Adding the 'inject' property to make clear that HTML injection is on

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -75,6 +75,7 @@ module.exports = require('./webpack.base.babel')({
       description: 'My React Boilerplate-based project!',
       background_color: '#fafafa',
       theme_color: '#b1624d',
+      inject: true,
       ios: true,
       icons: [
         {
