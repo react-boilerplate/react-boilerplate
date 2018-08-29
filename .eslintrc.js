2bcddb12cdad95cbed96720744772a977c53b9c2
Julien Benchetrit <julienbenchetrit@gmail.com>
2018-08-29 17:20:08 +0300

    Upgraded ESLint to 5.4.0 + other dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,9 +8,9 @@ module.exports = {
   extends: ['airbnb', 'prettier', 'prettier/react'],
   plugins: ['prettier', 'redux-saga', 'react', 'jsx-a11y'],
   env: {
+    jest: true,
     browser: true,
     node: true,
-    jest: true,
     es6: true,
   },
   parserOptions: {
@@ -42,7 +42,13 @@ module.exports = {
     ],
     'jsx-a11y/aria-props': 2,
     'jsx-a11y/heading-has-content': 0,
-    'jsx-a11y/label-has-for': 2,
+    "jsx-a11y/label-has-associated-control": [2, {
+      // NOTE: If this error triggers, either disable it or add
+      // your custom components, labels and attributes via these options
+      // See https://github.com/evcohen/eslint-plugin-jsx-a11y/blob/master/docs/rules/label-has-associated-control.md
+      "controlComponents": ["Input"],
+    }],
+    "jsx-a11y/label-has-for": 0,
     'jsx-a11y/mouse-events-have-key-events': 2,
     'jsx-a11y/role-has-required-aria-props': 2,
     'jsx-a11y/role-supports-aria-props': 2,
@@ -50,13 +56,16 @@ module.exports = {
     'newline-per-chained-call': 0,
     'no-confusing-arrow': 0,
     'no-console': 1,
+    'no-unused-vars': 2,
     'no-use-before-define': 0,
     'prefer-template': 2,
+    'react/destructuring-assignment': 0,
     'react/jsx-closing-tag-location': 0,
     'react/forbid-prop-types': 0,
     'react/jsx-first-prop-new-line': [2, 'multiline'],
     'react/jsx-filename-extension': 0,
     'react/jsx-no-target-blank': 0,
+    'react/jsx-uses-vars': 2,
     'react/require-default-props': 0,
     'react/require-extension': 0,
     'react/self-closing-comp': 0,
