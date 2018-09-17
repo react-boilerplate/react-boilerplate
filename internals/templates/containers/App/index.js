bf63da3523f43ad352cac23610e527be0c891826
Julien Benchetrit <julienben@users.noreply.github.com>
2018-09-18 08:49:53 +0900

    Small formatting update

--- internals/templates/containers/App/index.js
+++ internals/templates/containers/App/index.js
@@ -4,6 +4,7 @@
  *
  * This component is the skeleton around the actual pages, and should only
  * contain code that should be seen on all pages. (e.g. navigation bar)
+ *
  */
 
 import React from 'react';
