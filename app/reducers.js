3bb93ff09caf796fc99ceb136851ad5983391001
Julien Benchetrit <julienbenchetrit@gmail.com>
2018-10-10 13:18:49 +0300

    Readability and protection for createReducer function

--- app/reducers.js
+++ app/reducers.js
@@ -4,20 +4,32 @@
 
 import { combineReducers } from 'redux-immutable';
 import { connectRouter } from 'connected-react-router/immutable';
-import history from 'utils/history';
+import isObject from 'lodash/isObject';
+import isFunction from 'lodash/isFunction';
+import invariant from 'invariant';
 
+import history from 'utils/history';
 import globalReducer from 'containers/App/reducer';
 import languageProviderReducer from 'containers/LanguageProvider/reducer';
 
 /**
- * Creates the main reducer with the dynamically injected ones
+ * Merges the main reducer with the router state and dynamically injected reducers
  */
 export default function createReducer(injectedReducers) {
-  return connectRouter(history)(
-    combineReducers({
-      global: globalReducer,
-      language: languageProviderReducer,
-      ...injectedReducers,
-    }),
+  invariant(
+    isObject(injectedReducers) &&
+      Object.keys(injectedReducers) &&
+      Object.values(injectedReducers).every(isFunction),
+    "(app/reducers) createReducer: 'injectedReducers' must be an object of reducer functions",
   );
+
+  const rootReducer = combineReducers({
+    global: globalReducer,
+    language: languageProviderReducer,
+    ...injectedReducers,
+  });
+
+  // Wrap the root reducer and return a new root reducer with router state
+  const mergeWithRouterState = connectRouter(history);
+  return mergeWithRouterState(rootReducer);
 }
