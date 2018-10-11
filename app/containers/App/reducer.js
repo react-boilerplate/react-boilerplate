591d053aa5f63dbcd393bfd5024887b9ad68fe57
Julien Benchetrit <julienbenchetrit@gmail.com>
2018-10-11 13:15:49 +0300

    First stab at immer

--- app/containers/App/reducer.js
+++ app/containers/App/reducer.js
@@ -1,46 +1,46 @@
 /*
  * AppReducer
  *
- * The reducer takes care of our data. Using actions, we can change our
- * application state.
- * To add a new action, add it to the switch statement in the reducer function
+ * The reducer takes care of our data. Using actions, we can
+ * update our application state. To add a new action,
+ * add it to the switch statement in the reducer function
  *
- * Example:
- * case YOUR_ACTION_CONSTANT:
- *   return state.set('yourStateVariable', true);
  */
 
-import { fromJS } from 'immutable';
-
+import produce from 'immer';
 import { LOAD_REPOS_SUCCESS, LOAD_REPOS, LOAD_REPOS_ERROR } from './constants';
 
 // The initial state of the App
-const initialState = fromJS({
+export const initialState = {
   loading: false,
   error: false,
   currentUser: false,
   userData: {
     repositories: false,
   },
-});
+};
 
-function appReducer(state = initialState, action) {
-  switch (action.type) {
-    case LOAD_REPOS:
-      return state
-        .set('loading', true)
-        .set('error', false)
-        .setIn(['userData', 'repositories'], false);
-    case LOAD_REPOS_SUCCESS:
-      return state
-        .setIn(['userData', 'repositories'], action.repos)
-        .set('loading', false)
-        .set('currentUser', action.username);
-    case LOAD_REPOS_ERROR:
-      return state.set('error', action.error).set('loading', false);
-    default:
-      return state;
-  }
-}
+/* eslint-disable default-case, no-param-reassign */
+const appReducer = (state = initialState, action) =>
+  produce(state, draft => {
+    switch (action.type) {
+      case LOAD_REPOS:
+        draft.loading = true;
+        draft.error = false;
+        draft.userData.repositories = false;
+        break;
+
+      case LOAD_REPOS_SUCCESS:
+        draft.userData.repositories = action.repos;
+        draft.loading = false;
+        draft.currentUser = action.username;
+        break;
+
+      case LOAD_REPOS_ERROR:
+        draft.error = action.error;
+        draft.loading = false;
+        break;
+    }
+  });
 
 export default appReducer;
--- app/reducers.js
+++ app/reducers.js
@@ -2,8 +2,8 @@
  * Combine all reducers in this file and export the combined reducers.
  */
 
-import { combineReducers } from 'redux-immutable';
-import { connectRouter } from 'connected-react-router/immutable';
+import { combineReducers } from 'redux';
+import { connectRouter } from 'connected-react-router';
 
 import history from 'utils/history';
 import globalReducer from 'containers/App/reducer';
