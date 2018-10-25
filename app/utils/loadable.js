9b166cd46f47f64186519bc160c99e98bba21c75
Julien Benchetrit <julienbenchetrit@gmail.com>
2018-10-25 22:21:19 +0300

    Make fallback optional and fix template app

--- app/utils/loadable.js
+++ app/utils/loadable.js
@@ -1,6 +1,6 @@
 import React, { lazy, Suspense } from 'react';
 
-const loadable = (importFunc, { fallback }) => {
+const loadable = (importFunc, { fallback = null }) => {
   const LazyComponent = lazy(importFunc);
 
   return () => (
