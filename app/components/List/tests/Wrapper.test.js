f339385a166a5e689e0512a514431e05fbcc6940
Johan Meester <johan.meester71@gmail.com>
2018-11-20 13:14:21 +0100

    Replacing Enzyme with react-testing-library (#2452)
    * [Fix] Fixing issue were not all attributes are passed as required by 'containers/RepoListItem/IssueIcon' that wraps this component, according to test case 'should adopt any attribute'
    
    * Replacing 'enzyme' as testing utility with 'react-testing-library'.
    
    * Adapting the generator test templates for components and containers to use react-testing-library.
    
    * Adapting the testing documentation for use with react-testing-library
    
    * [Doc] Adding comments to the test templates to make clear that the standard snapshot test is skipped by default.
    
    * [Doc] Fixing a typing mistake.
    
    * Adding cleanup() from react-testing-library to tests that were missing it.
    
    * Adding some words to point out that the implemented tests in the example app deliberately show implementation variations in order to act as a guidance.

--- app/components/List/tests/Wrapper.test.js
+++ app/components/List/tests/Wrapper.test.js
@@ -1,32 +1,32 @@
 import React from 'react';
-import { mount } from 'enzyme';
-import { enzymeFind } from 'styled-components/test-utils';
+import { cleanup, render } from 'react-testing-library';
 
 import Wrapper from '../Wrapper';
 
 describe('<Wrapper />', () => {
+  afterEach(cleanup);
+
   it('should render an <div> tag', () => {
-    const wrapper = mount(<Wrapper />);
-    const renderedComponent = enzymeFind(wrapper, Wrapper);
-    expect(renderedComponent.type()).toEqual('div');
+    const { container } = render(<Wrapper />);
+    expect(container.firstElementChild.tagName).toEqual('DIV');
   });
 
-  it('should have a className attribute', () => {
-    const wrapper = mount(<Wrapper />);
-    const renderedComponent = enzymeFind(wrapper, Wrapper);
-    expect(renderedComponent.prop('className')).toBeDefined();
+  it('should have a class attribute', () => {
+    const { container } = render(<Wrapper />);
+    const element = container.firstElementChild;
+    expect(element.hasAttribute('class')).toBe(true);
   });
 
   it('should adopt a valid attribute', () => {
     const id = 'test';
-    const wrapper = mount(<Wrapper id={id} />);
-    const renderedComponent = enzymeFind(wrapper, Wrapper);
-    expect(renderedComponent.prop('id')).toEqual(id);
+    const { container } = render(<Wrapper id={id} />);
+    const element = container.firstElementChild;
+    expect(element.id).toEqual(id);
   });
 
   it('should not adopt an invalid attribute', () => {
-    const wrapper = mount(<Wrapper attribute="test" />);
-    const renderedComponent = enzymeFind(wrapper, Wrapper);
-    expect(renderedComponent.prop('attribute')).toBeUndefined();
+    const { container } = render(<Wrapper attribute="test" />);
+    const element = container.firstElementChild;
+    expect(element.hasAttribute('attribute')).toBe(false);
   });
 });
