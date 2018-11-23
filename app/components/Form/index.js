ac3d47c0048a285856eaf84ca9678d93ba088b1a
Fernando <ferdelamad@gmail.com>
2018-11-23 00:20:44 -0800

    Implemented AddItemForm container

--- app/components/Form/index.js
+++ app/components/Form/index.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 
 const Form = styled.form`
   border: 1px solid lightgrey;
-  margin: 0.5em;
   padding: 0.5em;
   text-align: center;
   width: 400px;
--- app/components/Input/index.js
+++ app/components/Input/index.js
@@ -5,11 +5,11 @@ import Input from './StyledInput';
 import Label from '../Label';
 
 const customInput = props => {
-  const { labelColor, label, inputType } = props;
+  const { labelColor, label, inputType, onChange, value } = props;
   return (
     <Label color={labelColor}>
       {label}
-      <Input type={inputType} />
+      <Input type={inputType} value={value} onChange={e => onChange(e)} />
     </Label>
   );
 };
@@ -18,10 +18,13 @@ customInput.propTypes = {
   labelColor: PropTypes.string,
   label: PropTypes.string,
   inputType: PropTypes.string,
+  onChange: PropTypes.func,
+  value: PropTypes.string,
 };
 
 customInput.defaultProps = {
   label: '',
+  value: '',
 };
 
 export default customInput;
