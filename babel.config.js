57ac64dc19842a6616b08e7f9a895ead3686ffec
Justyn Clark <ijustynclark@gmail.com>
2018-11-24 19:48:54 -0800

    Update babel.config.js
    fix jest test coverage errors

--- babel.config.js
+++ babel.config.js
@@ -24,6 +24,7 @@ module.exports = {
       ],
     },
     test: {
+      presets: ['@babel/preset-env', '@babel/preset-react'],
       plugins: [
         '@babel/plugin-transform-modules-commonjs',
         'dynamic-import-node',
