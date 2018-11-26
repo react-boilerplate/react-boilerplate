94b63937b4ce23a2d62451ad1b1675c28279dafd
Fernando <ferdelamad@gmail.com>
2018-11-26 15:08:29 -0800

    Refactored HomePage and AddItem initial tests

--- app/containers/DisplayItems/tests/selectors.test.js
+++
@@ -1,8 +0,0 @@
-// import { fromJS } from 'immutable';
-// import { selectDisplayItemsDomain } from '../selectors';
-
-// describe('selectDisplayItemsDomain', () => {
-//   it('Expect to have unit tests specified', () => {
-//     expect(true).toEqual(true);
-//   });
-// });
--- app/containers/HomePage/tests/index.test.js
+++ app/containers/HomePage/tests/index.test.js
@@ -3,6 +3,7 @@ import { FormattedMessage } from 'react-intl';
 import { shallow } from 'enzyme';
 
 import HomePage from '../index';
+import AddItem from '../../AddItem';
 import messages from '../messages';
 
 describe('<HomePage />', () => {
@@ -11,5 +12,28 @@ describe('<HomePage />', () => {
     expect(
       renderedComponent.contains(<FormattedMessage {...messages.header} />),
     ).toEqual(true);
+  });
+
+  it('should render a Wrapper Component', () => {
+    const renderedComponent = shallow(<HomePage />);
+    expect(renderedComponent.find('Wrapper').length).toBe(1);
+  });
+
+  it('should render addItem component', () => {
+    const renderedComponent = shallow(
+      <HomePage location={{ pathname: '/' }} />,
+    );
+    expect(renderedComponent.contains(<AddItem />)).toEqual(true);
+  });
+
+  it('should match render of HomePage', () => {
+    const renderedComponent = shallow(<HomePage />);
+    expect(renderedComponent).toMatchSnapshot();
+  });
+
+  it('should match render of addItem component', () => {
+    const renderedComponent = shallow(<HomePage />);
+    const addItemComponent = renderedComponent.find('AddItem');
+    expect(addItemComponent).toMatchSnapshot();
   });
 });
