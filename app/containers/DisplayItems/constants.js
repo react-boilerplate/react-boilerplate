bfdef8ec1c14bba56328c6e3698a66730fb26e89
Fernando <ferdelamad@gmail.com>
2018-11-25 20:14:42 -0800

    Added actions for DisplayItems container

--- app/containers/DisplayItems/constants.js
+++ app/containers/DisplayItems/constants.js
@@ -1,7 +1,3 @@
-/*
- *
- * DisplayItems constants
- *
- */
-
-export const DEFAULT_ACTION = 'app/DisplayItems/DEFAULT_ACTION';
+export const GET_ITEMS = 'app/DisplayItems/GET_ITEMS';
+export const GET_ITEMS_SUCCESS = 'app/DisplayItems/GET_ITEMS_SUCCESS';
+export const GET_ITEMS_ERROR = 'app/DisplayItems/GET_ITEMS_ERROR';
