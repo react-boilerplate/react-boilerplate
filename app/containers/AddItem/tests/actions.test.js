f55e3aa281961a7eb96857f16ca04237ade4adb0
Fernando <ferdelamad@gmail.com>
2018-11-26 15:31:37 -0800

    Implemented testing for addItem actions

--- app/containers/AddItem/tests/actions.test.js
+++ app/containers/AddItem/tests/actions.test.js
@@ -1,5 +1,5 @@
-import { addItem } from '../actions';
-import { ADD_ITEM } from '../constants';
+import * as actions from '../actions';
+import { ADD_ITEM, ADD_ITEM_SUCCESS, ADD_ITEM_ERROR } from '../constants';
 
 describe('AddItem actions', () => {
   describe('add item action', () => {
@@ -7,7 +7,25 @@ describe('AddItem actions', () => {
       const expected = {
         type: ADD_ITEM,
       };
-      expect(addItem()).toEqual(expected);
+      expect(actions.addItem()).toEqual(expected);
+    });
+  });
+
+  describe('add item success action', () => {
+    it('has a type of ADD_ITEM_SUCCESS', () => {
+      const expected = {
+        type: ADD_ITEM_SUCCESS,
+      };
+      expect(actions.addItemSuccess()).toEqual(expected);
+    });
+  });
+
+  describe('add item error action', () => {
+    it('has a type of ADD_ITEM_ERROR', () => {
+      const expected = {
+        type: ADD_ITEM_ERROR,
+      };
+      expect(actions.addItemError()).toEqual(expected);
     });
   });
 });
