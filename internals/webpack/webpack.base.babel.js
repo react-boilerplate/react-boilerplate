a8dddc1bfa9bee758ceb100dec9da9501ddfcb02
Marcellin Nshimiyimana <mn2587@columbia.edu>
2018-11-28 01:33:53 -0500

    ignore change to webpack base file

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -5,9 +5,6 @@
 const path = require('path');
 const webpack = require('webpack');
 
-const BUILD_FOLDER_PATH = process.env.BUILD_FOLDER_PATH || 'build';
-const PUBLIC_PATH = process.env.PUBLIC_PATH || '/';
-
 // Remove this line once the following warning goes away (it was meant for webpack loader authors not users):
 // 'DeprecationWarning: loaderUtils.parseQuery() received a non-string value which can be problematic,
 // see https://github.com/webpack/loader-utils/issues/56 parseQuery() will be replaced with getOptions()
@@ -20,8 +17,8 @@ module.exports = options => ({
   output: Object.assign(
     {
       // Compile into js/build.js
-      path: path.resolve(process.cwd(), BUILD_FOLDER_PATH),
-      publicPath: PUBLIC_PATH,
+      path: path.resolve(process.cwd(), 'build'),
+      publicPath: '/',
     },
     options.output,
   ), // Merge with env dependent settings
@@ -120,9 +117,10 @@ module.exports = options => ({
     // Always expose NODE_ENV to webpack, in order to use `process.env.NODE_ENV`
     // inside your code for any environment checks; Terser will automatically
     // drop any unreachable code.
-    new webpack.EnvironmentPlugin({
-      NODE_ENV: 'development',
-      PUBLIC_PATH: '/',
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
     }),
   ]),
   resolve: {
