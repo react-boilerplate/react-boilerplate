4ba775bea36233efec8480ed3ab090ee51e44149
Marcellin Nshimiyimana <mn2587@columbia.edu>
2018-11-28 01:56:25 -0500

    replace DefinePlugin with EnvironmentPlugin
    You can read more on this by following this link https://medium.com/friday-knowledge/webpack-compiler-placeholders-3598f5f723b9

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -117,10 +117,8 @@ module.exports = options => ({
     // Always expose NODE_ENV to webpack, in order to use `process.env.NODE_ENV`
     // inside your code for any environment checks; Terser will automatically
     // drop any unreachable code.
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-      },
+    new webpack.EnvironmentPlugin({
+      NODE_ENV: 'development',
     }),
   ]),
   resolve: {
