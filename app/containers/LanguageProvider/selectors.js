7da2bd5c4a8fcbdb66256e46e01af7592673768d
Julien Benchetrit <julienbenchetrit@gmail.com>
2019-02-23 18:58:46 +0200

    Little bit more fixin'

--- app/containers/LanguageProvider/selectors.js
+++ app/containers/LanguageProvider/selectors.js
@@ -1,16 +1,20 @@
 import { createSelector } from 'reselect';
+import { initialState } from './reducer';
 
 /**
  * Direct selector to the language domain
  */
 
-const selectLanguage = state => state.language;
+const selectLanguage = state => state.language || initialState;
 
 /**
  * Select the language locale
  */
 
 const makeSelectLocale = () =>
-  createSelector(selectLanguage, languageState => languageState.locale);
+  createSelector(
+    selectLanguage,
+    languageState => languageState.locale,
+  );
 
 export { selectLanguage, makeSelectLocale };
