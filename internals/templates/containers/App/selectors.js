d18b7fcc35a5480477ac855839a52a287316998f
Julien Benchetrit <julienbenchetrit@gmail.com>
2019-02-23 19:02:35 +0200

    Linting fix

--- internals/templates/containers/App/selectors.js
+++ internals/templates/containers/App/selectors.js
@@ -3,6 +3,9 @@ import { createSelector } from 'reselect';
 const selectRouter = state => state.router;
 
 const makeSelectLocation = () =>
-  createSelector(selectRouter, routerState => routerState.location);
+  createSelector(
+    selectRouter,
+    routerState => routerState.location,
+  );
 
 export { makeSelectLocation };
