3f4d60840629eb6e9795718f9b5d9a557076f11e
Julien Benchetrit <julienbenchetrit@gmail.com>
2019-02-23 13:07:26 +0200

    Mucho maintenance

--- jest.config.js
+++ jest.config.js
@@ -21,7 +21,7 @@ module.exports = {
     '.*\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
       '<rootDir>/internals/mocks/image.js',
   },
-  setupTestFrameworkScriptFile: '<rootDir>/internals/testing/test-bundler.js',
+  setupFilesAfterEnv: ['<rootDir>/internals/testing/test-bundler.js'],
   setupFiles: ['raf/polyfill', '<rootDir>/internals/testing/enzyme-setup.js'],
   testRegex: 'tests/.*\\.test\\.js$',
   snapshotSerializers: ['enzyme-to-json/serializer'],
