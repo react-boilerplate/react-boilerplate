4d7c55b7319a0c8d466bf742a2bf8cb6b848c1ca
Julien Benchetrit <julienbenchetrit@gmail.com>
2019-02-23 18:52:12 +0200

    Partial fix for Immer issue

--- app/containers/HomePage/selectors.js
+++ app/containers/HomePage/selectors.js
@@ -3,10 +3,14 @@
  */
 
 import { createSelector } from 'reselect';
+import initialState from './reducer';
 
-const selectHome = state => state.home;
+const selectHome = state => state.home || initialState;
 
 const makeSelectUsername = () =>
-  createSelector(selectHome, homeState => homeState.username);
+  createSelector(
+    selectHome,
+    homeState => homeState.username,
+  );
 
 export { selectHome, makeSelectUsername };
