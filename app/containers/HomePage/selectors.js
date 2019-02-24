5c6d349261556fd402ea01f664569911859a3b8e
Julien Benchetrit <julienbenchetrit@gmail.com>
2019-02-24 13:13:02 +0200

    Bug fix + lock updates

--- app/containers/HomePage/selectors.js
+++ app/containers/HomePage/selectors.js
@@ -3,7 +3,7 @@
  */
 
 import { createSelector } from 'reselect';
-import initialState from './reducer';
+import { initialState } from './reducer';
 
 const selectHome = state => state.home || initialState;
 
