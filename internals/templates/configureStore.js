854b3226ce870557a9787897a855524c18a20017
Julien Benchetrit <julienbenchetrit@gmail.com>
2019-03-07 00:15:42 +0100

    Update configureStore.js from template app

--- internals/templates/configureStore.js
+++ internals/templates/configureStore.js
@@ -9,18 +9,21 @@ import createReducer from './reducers';
 
 export default function configureStore(initialState = {}, history) {
   let composeEnhancers = compose;
-  let reduxSagaMonitorOptions = {};
+  const reduxSagaMonitorOptions = {};
 
-  // If Redux and Saga DevTools Extension are installed use them
+  // If Redux Dev Tools and Saga Dev Tools Extensions are installed, enable them
   /* istanbul ignore next */
   if (process.env.NODE_ENV !== 'production' && typeof window === 'object') {
     /* eslint-disable no-underscore-dangle */
     if (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__)
       composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({});
-    if (window.__SAGA_MONITOR_EXTENSION__)
-      reduxSagaMonitorOptions = {
-        sagaMonitor: window.__SAGA_MONITOR_EXTENSION__,
-      };
+
+    // NOTE: Uncomment the code below to restore support for Redux Saga
+    // Dev Tools once it supports redux-saga version 1.x.x
+    // if (window.__SAGA_MONITOR_EXTENSION__)
+    //   reduxSagaMonitorOptions = {
+    //     sagaMonitor: window.__SAGA_MONITOR_EXTENSION__,
+    //   };
     /* eslint-enable */
   }
 
