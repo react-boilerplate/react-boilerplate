7743e6a40cc9716b1e46d17d80268b9741ec2e9c
kashaf <akashaf@hermo.my>
2019-04-16 17:40:49 +0800

    linter update from 2 spaces to 4 spaces

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -10,16 +10,14 @@
  */
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
 
 /* eslint-disable react/prefer-stateless-function */
 export default class HomePage extends React.PureComponent {
     render() {
         return (
-            <h1>
-                <FormattedMessage {...messages.header} />
-            </h1>
+            <div>
+                <h1>home page container</h1>
+            </div>
         );
     }
 }
