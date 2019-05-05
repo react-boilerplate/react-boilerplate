4fe28d8eb0b6dfb4bb88dc05582b33d9b3107dd1
Julien Benchetrit <julienbenchetrit@gmail.com>
2019-05-05 12:51:46 +0300

    Removed deprecated @babel/polyfill and using react-app-polyfill exclusively now

--- internals/webpack/webpack.prod.babel.js
+++ internals/webpack/webpack.prod.babel.js
@@ -11,10 +11,7 @@ module.exports = require('./webpack.base.babel')({
   mode: 'production',
 
   // In production, we skip all hot-reloading stuff
-  entry: [
-    require.resolve('react-app-polyfill/ie11'),
-    path.join(process.cwd(), 'app/app.js'),
-  ],
+  entry: path.join(process.cwd(), 'app/app.js'),
 
   // Utilize long-term caching by adding content hashes (not compilation hashes) to compiled assets
   output: {
