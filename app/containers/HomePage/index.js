3fecd8dfbcb01da52866e0a7bdfaf9621292ea95
Julien Benchetrit <julienbenchetrit@gmail.com>
2019-05-07 16:30:47 +0300

    Demo for react-redux hooks

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -5,12 +5,9 @@
  */
 
 import React, { useEffect, memo } from 'react';
-import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
 import { FormattedMessage } from 'react-intl';
-import { connect } from 'react-redux';
-import { compose } from 'redux';
-import { createStructuredSelector } from 'reselect';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { useInjectReducer } from 'utils/injectReducer';
 import { useInjectSaga } from 'utils/injectSaga';
@@ -35,14 +32,20 @@ import saga from './saga';
 
 const key = 'home';
 
-export function HomePage({
-  username,
-  loading,
-  error,
-  repos,
-  onSubmitForm,
-  onChangeUsername,
-}) {
+export function HomePage() {
+  const repos = useSelector(makeSelectRepos());
+  const username = useSelector(makeSelectUsername());
+  const loading = useSelector(makeSelectLoading());
+  const error = useSelector(makeSelectError());
+
+  const dispatch = useDispatch();
+
+  const onChangeUsername = evt => dispatch(changeUsername(evt.target.value));
+  const onSubmitForm = evt => {
+    if (evt !== undefined && evt.preventDefault) evt.preventDefault();
+    dispatch(loadRepos());
+  };
+
   useInjectReducer({ key, reducer });
   useInjectSaga({ key, saga });
 
@@ -101,38 +104,4 @@ export function HomePage({
   );
 }
 
-HomePage.propTypes = {
-  loading: PropTypes.bool,
-  error: PropTypes.oneOfType([PropTypes.object, PropTypes.bool]),
-  repos: PropTypes.oneOfType([PropTypes.array, PropTypes.bool]),
-  onSubmitForm: PropTypes.func,
-  username: PropTypes.string,
-  onChangeUsername: PropTypes.func,
-};
-
-const mapStateToProps = createStructuredSelector({
-  repos: makeSelectRepos(),
-  username: makeSelectUsername(),
-  loading: makeSelectLoading(),
-  error: makeSelectError(),
-});
-
-export function mapDispatchToProps(dispatch) {
-  return {
-    onChangeUsername: evt => dispatch(changeUsername(evt.target.value)),
-    onSubmitForm: evt => {
-      if (evt !== undefined && evt.preventDefault) evt.preventDefault();
-      dispatch(loadRepos());
-    },
-  };
-}
-
-const withConnect = connect(
-  mapStateToProps,
-  mapDispatchToProps,
-);
-
-export default compose(
-  withConnect,
-  memo,
-)(HomePage);
+export default memo(HomePage);
