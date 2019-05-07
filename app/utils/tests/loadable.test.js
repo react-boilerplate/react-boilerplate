b0b6ebc1ab6486dcf8ff589620a37080a3cf5446
Julien Benchetrit <julienben@users.noreply.github.com>
2019-05-07 17:25:05 +0300

    Tests for loadable.js (#2647)

---
+++ app/utils/tests/loadable.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { render } from 'react-testing-library';
+import loadable from 'utils/loadable';
+
+const LoadingIndicator = () => <div>Loading</div>;
+
+const LazyComponent = loadable(() => import('./loadable.index'));
+
+const LazyComponentWithFallback = loadable(() => import('./loadable.index'), {
+  fallback: <LoadingIndicator />,
+});
+
+describe('loadable', () => {
+  it('should render null by default', () => {
+    const {
+      container: { firstChild },
+    } = render(<LazyComponent />);
+    expect(firstChild).toMatchSnapshot();
+  });
+
+  it('should render fallback if given one', () => {
+    const {
+      container: { firstChild },
+    } = render(<LazyComponentWithFallback />);
+    expect(firstChild).toMatchSnapshot();
+  });
+
+  it('should render LazyComponent after waiting for it to load', async () => {
+    const {
+      container: { firstChild },
+    } = render(<LazyComponent />);
+    await LazyComponent;
+    expect(firstChild).toMatchSnapshot();
+  });
+});
