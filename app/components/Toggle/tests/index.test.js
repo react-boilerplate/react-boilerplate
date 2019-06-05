5330b209c6f73a383d7d725fbd401f586cbab1cd
Sarhan Aissi <tux-tn@users.noreply.github.com>
2019-06-05 12:50:58 +0100

    Upgrade to react-testing-library 8 (#2672)
    * Bump npm deps due to npm audit issues
    
    * Upgrade to react-testing-library 8
    
    * Pin @testing-library/react

--- app/components/Toggle/tests/index.test.js
+++ app/components/Toggle/tests/index.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from 'react-testing-library';
+import { render } from '@testing-library/react';
 import { IntlProvider, defineMessages } from 'react-intl';
 
 import Toggle from '../index';
--- app/containers/HomePage/tests/index.test.js
+++ app/containers/HomePage/tests/index.test.js
@@ -3,7 +3,7 @@
  */
 
 import React from 'react';
-import { render } from 'react-testing-library';
+import { render } from '@testing-library/react';
 import { IntlProvider } from 'react-intl';
 import { Provider } from 'react-redux';
 import { browserHistory } from 'react-router-dom';
