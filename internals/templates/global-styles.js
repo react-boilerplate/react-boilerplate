9dc6db17e20073171f6d99847e598e5e20fd9359
Julien Benchetrit <julienben@users.noreply.github.com>
2019-06-28 17:28:56 +0200

    Maintenance and related updates/tweaks (#2685)

--- internals/templates/global-styles.js
+++ internals/templates/global-styles.js
@@ -11,10 +11,6 @@ const GlobalStyle = createGlobalStyle`
     font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
   }
 
-  body.fontLoaded {
-    font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
-  }
-
   #app {
     background-color: #fafafa;
     min-height: 100%;
@@ -25,6 +21,11 @@ const GlobalStyle = createGlobalStyle`
   label {
     font-family: Georgia, Times, 'Times New Roman', serif;
     line-height: 1.5em;
+  }
+
+  input, select {
+    font-family: inherit;
+    font-size: inherit;
   }
 `;
 
