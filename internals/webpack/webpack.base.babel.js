b425f52029f4120acca588d9fe9a462e41a76cf4
Julien Benchetrit <julienbenchetrit@gmail.com>
2019-07-09 12:05:25 +0300

    Maintenance

--- internals/webpack/webpack.base.babel.js
+++ internals/webpack/webpack.base.babel.js
@@ -52,7 +52,6 @@ module.exports = options => ({
             options: {
               // Inline files smaller than 10 kB
               limit: 10 * 1024,
-              noquotes: true,
             },
           },
         ],
