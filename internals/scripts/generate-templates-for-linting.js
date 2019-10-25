1436416e2bc8d63cb01f8f8aae4528f6f9c5fee1
Julien Benchetrit <julienben@users.noreply.github.com>
2019-10-25 13:38:41 +0300

    Setup updates (#2783)
    * No need to use shjs here (no?)
    
    * This file is never used as the clean script copies it from the main app
    
    * Centralized getting required npm/node versions and update npm version check script
    
    * Extract some of the setup functions to a separate file
    
    * Use addXmark like everywhere else
    
    * Make repo deletion message clearer
    
    * Add compare-versions to presetup script (#2776)
    
    * Formatting
    
    * Remove unnecessary npm version check script

--- internals/scripts/generate-templates-for-linting.js
+++ internals/scripts/generate-templates-for-linting.js
@@ -14,7 +14,7 @@ const rimraf = require('rimraf');
 const shell = require('shelljs');
 
 const addCheckmark = require('./helpers/checkmark');
-const xmark = require('./helpers/xmark');
+const addXmark = require('./helpers/xmark');
 
 /**
  * Every generated component/container is preceded by this
@@ -91,7 +91,7 @@ function reportSuccess(message) {
  */
 function reportErrors(reason) {
   // TODO Replace with our own helpers/log that is guaranteed to be blocking?
-  xmark(() => console.error(chalk.red(` ${reason}`)));
+  addXmark(() => console.error(chalk.red(` ${reason}`)));
   process.exit(1);
 }
 
@@ -373,7 +373,7 @@ async function generateLanguage(language) {
 /**
  * Run
  */
-(async function () {
+(async function() {
   await generateComponents([
     { kind: 'component', name: 'Component', memo: false },
     { kind: 'component', name: 'MemoizedComponent', memo: true },
