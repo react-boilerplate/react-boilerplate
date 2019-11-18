ae786009469de3a5de6b35ecfc5b73b2807ba21a
Mark Erikson <mark@isquaredsoftware.com>
2019-11-17 22:26:50 -0500

    Replace references to plain Redux package

--- app/reducers.js
+++ app/reducers.js
@@ -2,7 +2,7 @@
  * Combine all reducers in this file and export the combined reducers.
  */
 
-import { combineReducers } from 'redux';
+import { combineReducers } from '@reduxjs/toolkit';
 import { connectRouter } from 'connected-react-router';
 
 import history from 'utils/history';
