2c9b5330ac1c0b2ec7f235cd7e21274ef793d706
receptiryaki <recep.tiryaki@gmail.com>
2020-04-12 19:08:15 +0300

    colors defined.

--- src/styles/theme/themes.ts
+++ src/styles/theme/themes.ts
@@ -1,18 +1,26 @@
-const defaultTheme = {
-  primary: '',
-  componentBackground: '',
-  componentBackgroundSecondary: '',
+const lightTheme = {
+  primary: 'rgba(215, 113, 88, 1)',
+  text: 'rgba(58, 52, 51, 1)',
+  textSecondary: 'rgba(58, 52, 51, 0.7)',
+  background: 'rgba(255, 255, 255, 1)',
+  backgroundVariant: 'rgba(251, 249, 249, 1)',
+  border: 'rgba(58, 52, 51, 0.12)',
+  borderLight: 'rgba(58, 52, 51, 0.05)',
 };
 
 const darkTheme: Theme = {
-  primary: '',
-  componentBackground: '',
-  componentBackgroundSecondary: '',
+  primary: 'rgba(200, 104, 21, 1)',
+  text: 'rgba(241, 233, 231, 1)',
+  textSecondary: 'rgba(241, 233, 231, 0.6)',
+  background: 'rgba(0, 0, 0, 1)',
+  backgroundVariant: 'rgba(28, 26, 26, 1)',
+  border: 'rgba(241, 233, 231, 0.12)',
+  borderLight: 'rgba(241, 233, 231, 0.5)',
 };
 
-export type Theme = typeof defaultTheme;
+export type Theme = typeof lightTheme;
 
 export const themes = {
-  default: defaultTheme,
+  default: lightTheme,
   dark: darkTheme,
 };
