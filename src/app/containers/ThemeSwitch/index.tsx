c30abfe9a6a469e78862ec41137190fb11daf1a8
receptiryaki <recep.tiryaki@gmail.com>
2020-04-17 11:50:59 +0300

    radio component moved in to main components.

--- src/app/containers/ThemeSwitch/index.tsx
+++ src/app/containers/ThemeSwitch/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FormLabel } from 'app/components/FormLabel';
-import { Radio } from './components/Radio';
+import { Radio } from 'app/components/Radio';
 import styled from 'styled-components/macro';
 import { changeTheme, selectThemeKey } from 'styles/theme/slice';
 import { useDispatch, useSelector } from 'react-redux';
