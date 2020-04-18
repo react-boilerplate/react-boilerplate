dfc2cd476faf7b343097d5f70c0d9c4fadea40f6
Can Sahin <cann2005@gmail.com>
2020-04-18 13:05:48 +0300

    starting with tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,16 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
+// react-testing-library renders your components to document.body,
+// this adds jest-dom's custom assertions
 import '@testing-library/jest-dom/extend-expect';
+
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
+
+import 'jest-styled-components';
+
+const localStorageMock = {
+  getItem: jest.fn(),
+  setItem: jest.fn(),
+  removeItem: jest.fn(),
+  clear: jest.fn(),
+};
+(global as any).localStorage = localStorageMock;
