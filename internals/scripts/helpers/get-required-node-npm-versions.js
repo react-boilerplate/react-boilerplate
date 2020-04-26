dc880e1dd4e7ce110f5c26e566281876f6c9afdd
yasser <spookyframework@gmail.com>
2020-04-26 12:05:47 +0200

    deleteget-required-node-npm-versions.js

--- internals/scripts/helpers/get-required-node-npm-versions.js
+++
@@ -1,8 +0,0 @@
-const {
-  engines: { node, npm },
-} = require('../../../package.json');
-
-module.exports = {
-  requiredNodeVersion: node.match(/([0-9.]+)/g)[0],
-  requiredNpmVersion: npm.match(/([0-9.]+)/g)[0],
-};
