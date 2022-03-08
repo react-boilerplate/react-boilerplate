8799dacaa10b4eeca3374c9bf186fc9559da2ed6
567 <567@gustochain.com>
2022-03-08 14:35:55 +0800

    zh.json

--- app/i18n.js
+++ app/i18n.js
@@ -9,40 +9,45 @@
  */
 const addLocaleData = require('react-intl').addLocaleData; //eslint-disable-line
 const enLocaleData = require('react-intl/locale-data/en');
+const zhLocaleData = require('react-intl/locale-data/zh');
 const deLocaleData = require('react-intl/locale-data/de');
 
 const enTranslationMessages = require('./translations/en.json');
+const zhTranslationMessages = require('./translations/zh.json');
 const deTranslationMessages = require('./translations/de.json');
 
 addLocaleData(enLocaleData);
+addLocaleData(zhLocaleData);
 addLocaleData(deLocaleData);
 
-const DEFAULT_LOCALE = 'en';
+const DEFAULT_LOCALE = 'zh';
 
 // prettier-ignore
 const appLocales = [
-  'en',
-  'de',
+    'en',
+    'zh',
+    'de',
 ];
 
 const formatTranslationMessages = (locale, messages) => {
-  const defaultFormattedMessages =
-    locale !== DEFAULT_LOCALE
-      ? formatTranslationMessages(DEFAULT_LOCALE, enTranslationMessages)
-      : {};
-  const flattenFormattedMessages = (formattedMessages, key) => {
-    const formattedMessage =
-      !messages[key] && locale !== DEFAULT_LOCALE
-        ? defaultFormattedMessages[key]
-        : messages[key];
-    return Object.assign(formattedMessages, { [key]: formattedMessage });
-  };
-  return Object.keys(messages).reduce(flattenFormattedMessages, {});
+    const defaultFormattedMessages =
+        locale !== DEFAULT_LOCALE ?
+        formatTranslationMessages(DEFAULT_LOCALE, enTranslationMessages) : {};
+    const flattenFormattedMessages = (formattedMessages, key) => {
+        const formattedMessage = !messages[key] && locale !== DEFAULT_LOCALE ?
+            defaultFormattedMessages[key] :
+            messages[key];
+        return Object.assign(formattedMessages, {
+            [key]: formattedMessage
+        });
+    };
+    return Object.keys(messages).reduce(flattenFormattedMessages, {});
 };
 
 const translationMessages = {
-  en: formatTranslationMessages('en', enTranslationMessages),
-  de: formatTranslationMessages('de', deTranslationMessages),
+    en: formatTranslationMessages('en', enTranslationMessages),
+    zh: formatTranslationMessages('zh', zhTranslationMessages),
+    de: formatTranslationMessages('de', deTranslationMessages),
 };
 
 exports.appLocales = appLocales;
